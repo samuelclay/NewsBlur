1bc2766003e8ee032e7034674c4a898883abab5c
Samuel Clay <samuel@ofbrooklyn.com>
2009-06-18 02:43:10 +0000

    More media_url changes

--- settings.py
+++ settings.py
@@ -63,7 +63,7 @@ USE_I18N = False
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://www.newsblur.com/media/media'
+MEDIA_URL = 'http://www.newsblur.com/media/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
