a29ed65caf7a0d0bc6ac6fce0e8c87e2485b8797
Samuel Clay <samuel@ofbrooklyn.com>
2010-04-03 20:06:17 +0000

    Settings update.

--- settings.py
+++ settings.py
@@ -59,11 +59,9 @@ if PRODUCTION:
             'ENGINE': 'django.db.backends.mysql',
             'USER': 'newsblur',
             'PASSWORD': '',
-            'HOST': 'localhost'
+            'HOST': '127.0.0.1'
         }
     }
-    DATABASE_HOST = 'localhost'
-    DATABASE_PORT = ''
     # Absolute path to the directory that holds media.
     # Example: "/Users/media/media.lawrence.com/"
     MEDIA_URL = 'http://www.newsblur.com/media/'
@@ -82,7 +80,6 @@ elif STAGING:
             'USER': 'newsblur',
             'PASSWORD': '',
             'HOST': 'localhost',
-            'HOST': 'localhost'
         }
     }
     DATABASE_HOST = 'localhost'
