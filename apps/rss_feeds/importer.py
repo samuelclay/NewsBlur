00ed6294432374e311247abffa43d15dab60e63b
Samuel Clay <samuel@ofbrooklyn.com>
2010-04-23 10:44:46 -0400

    Ignoring fix urls path for now.

--- apps/rss_feeds/importer.py
+++ apps/rss_feeds/importer.py
@@ -1,6 +1,7 @@
 import urllib2
 import logging
 import re
+import urlparse
 import multiprocessing
 
 class PageImporter(object):
@@ -26,14 +27,39 @@ class PageImporter(object):
             self.save_page(html)
     
     def rewrite_page(self, response):
+        BASE_RE = re.compile(r'<head(.*?\>)', re.I)
         base_code = u'<base href="%s" />' % (self.feed.feed_link,)
         try:
-            html = re.sub(r'<head(.*?\>)', r'<head\1 '+base_code, response)
+            html = BASE_RE.sub(r'<head\1 '+base_code, response)
         except:
             response = response.decode('latin1').encode('utf-8')
-            html = re.sub(r'<head(.*?\>)', r'<head\1 '+base_code, response)
+            html = BASE_RE.sub(r'<head\1 '+base_code, response)
+        
+        # html = self.fix_urls(html)
         
         return html
+
+    def fix_urls(self, document):
+        # BEWARE: This will rewrite URLs inside of <script> tags. You know, like
+        # Google Analytics. Ugh.
+        
+        FIND_RE = re.compile(r'\b(href|src)\s*=\s*("[^"]*"|\'[^\']*\'|[^"\'<>=\s]+)')
+        ret = []
+        last_end = 0
+        
+        for match in FIND_RE.finditer(document):
+            url = match.group(2)
+            if url[0] in "\"'":
+                url = url.strip(url[0])
+            parsed = urlparse.urlparse(url)
+            if parsed.scheme == parsed.netloc == '': #relative to domain
+                url = urlparse.urljoin(self.feed.feed_link, url)
+                ret.append(document[last_end:match.start(2)])
+                ret.append('"%s"' % (url,))
+                last_end = match.end(2)
+        ret.append(document[last_end:])
+        
+        return ''.join(ret)
         
     def save_page(self, html):
         self.feed.page_data = html
