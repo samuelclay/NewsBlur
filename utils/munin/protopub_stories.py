2c2a0e80821976d9afa305ff90a8d681f542c3f1
Samuel Clay <samuel@ofbrooklyn.com>
2010-05-20 16:54:41 -0400

    Adding stories to munin.

---
+++ utils/munin/protopub_stories.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+
+from utils.munin.base import MuninGraph
+from apps.rss_feeds.models import Story, Tag, Author
+from apps.reader.models import UserStory
+
+graph_config = {
+    'graph_category' : 'Protopub',
+    'graph_title' : 'Protopub Stories',
+    'graph_vlabel' : 'Stories',
+    'stories.label': 'stories',
+    'tags.label': 'tags',
+    'authors.label': 'authors',
+    'read_stories.label': 'read_stories',
+}
+
+metrics = {
+    'stories': Story.objects.count(),
+    'tags': Tag.objects.count(),
+    'authors': Author.objects.count(),
+    'read_stories': UserStory.objects.count(),
+}
+
+if __name__ == '__main__':
+    MuninGraph(graph_config, metrics).run()
