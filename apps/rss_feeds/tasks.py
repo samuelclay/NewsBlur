ab3d0520654ff174c1723759c423c6b4ede581d2
Samuel Clay <samuel@ofbrooklyn.com>
2010-09-01 10:37:58 -0400

    Using a list of feed_pks instead of one. Less task overhead.

--- apps/rss_feeds/tasks.py
+++ apps/rss_feeds/tasks.py
@@ -5,7 +5,11 @@ class RefreshFeed(Task):
     name = 'refresh-feed'
     max_retries = 0
 
-    def run(self, feed_pk, **kwargs):
-        feed = Feed.objects.get(pk=feed_pk)
-        feed.update()
+    def run(self, feed_pks, **kwargs):
+        if not isinstance(feed_pks, list):
+            feed_pks = [feed_pks]
+            
+        for feed_pk in feed_pks:
+            feed = Feed.objects.get(pk=feed_pk)
+            feed.update()
 
