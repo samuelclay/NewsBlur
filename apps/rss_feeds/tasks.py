c42cabcae2a1a140958ef32ad6c3de1691a8bc1f
Samuel Clay <samuel@ofbrooklyn.com>
2010-09-01 08:19:58 -0400

    Celery options for performance.

--- apps/rss_feeds/tasks.py
+++ apps/rss_feeds/tasks.py
@@ -4,6 +4,7 @@ from apps.rss_feeds.models import Feed
 class RefreshFeed(Task):
     name = 'refresh-feed'
     max_retries = 0
+    ignore_result = True
 
     def run(self, feed_pk, **kwargs):
         feed = Feed.objects.get(pk=feed_pk)
