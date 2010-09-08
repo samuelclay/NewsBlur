2b9ec596f9f88f5fb608e004d5fe60cfd88cb635
Samuel Clay <samuel@ofbrooklyn.com>
2010-09-08 10:52:04 -0700

    Adding indexes to dates on models.

--- utils/munin/newsblur_updates.py
+++ utils/munin/newsblur_updates.py
@@ -11,9 +11,11 @@ graph_config = {
     'feeds_fetched.label': 'Fetched feeds last hour',
 }
 
+hour_ago = datetime.datetime.now() - datetime.timedelta(hours=1)
+
 metrics = {
     'update_queue': Feed.objects.filter(next_scheduled_update__lte=datetime.datetime.now(), active=True).count(),
-    'feeds_fetched': Feed.objects.filter(last_update__gte=datetime.datetime.now()-datetime.timedelta(hours=1))
+    'feeds_fetched': Feed.objects.filter(last_update__gte=hour_ago).count()
 }
 
 if __name__ == '__main__':
