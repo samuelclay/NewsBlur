6a0f85df0fa8ec1c1d3c8a734e3d0c219f023d5a
Samuel Clay <samuel@ofbrooklyn.com>
2010-09-29 17:51:47 -0400

    Tossing half the graphs for loadtimes. I don't need daily averages, just hourly is fine.

--- utils/munin/newsblur_loadtimes.py
+++ utils/munin/newsblur_loadtimes.py
@@ -13,26 +13,18 @@ graph_config = {
     'feed_loadtimes_avg_hour.label': 'Feed Loadtimes Avg (Hour)',
     'feed_loadtimes_min_hour.label': 'Feed Loadtimes Min (Hour)',
     'feed_loadtimes_max_hour.label': 'Feed Loadtimes Max (Hour)',
-    'feed_loadtimes_avg_day.label': 'Feed Loadtimes Avg (Day)',
-    'feed_loadtimes_min_day.label': 'Feed Loadtimes Min (Day)',
-    'feed_loadtimes_max_day.label': 'Feed Loadtimes Max (Day)',
     'feeds_loaded_hour.label': 'Feeds Loaded (Hour)',
 }
 
-day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
 hour_ago = datetime.datetime.now() - datetime.timedelta(minutes=60)
 
 averages = dict(avg=Avg('loadtime'), max=Max('loadtime'), min=Min('loadtime'), count=Count('loadtime'))
-day = FeedLoadtime.objects.filter(date_accessed__gte=day_ago).aggregate(**averages)
 hour = FeedLoadtime.objects.filter(date_accessed__gte=hour_ago).aggregate(**averages)
 
 metrics = {
     'feed_loadtimes_avg_hour': hour['avg'],
     'feed_loadtimes_min_hour': hour['min'],
     'feed_loadtimes_max_hour': hour['max'],
-    'feed_loadtimes_avg_day': day['avg'],
-    'feed_loadtimes_min_day': day['min'],
-    'feed_loadtimes_max_day': day['max'],
     'feeds_loaded_hour': hour['count'],
 }
 
