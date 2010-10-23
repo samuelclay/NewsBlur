8448a2cf084cdeb336f314b551b1f815f8e1e6fc
Samuel Clay <samuel@ofbrooklyn.com>
2010-10-23 12:29:18 -0400

    Logging for new premium accounts. Emails + logs.

--- apps/profile/models.py
+++ apps/profile/models.py
@@ -2,8 +2,10 @@ import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.core.mail import mail_admins
 from apps.reader.models import UserSubscription
 from paypal.standard.ipn.signals import subscription_signup
+from utils import log as logging
      
 class Profile(models.Model):
     user = models.OneToOneField(User, unique=True, related_name="profile")
@@ -27,6 +29,15 @@ class Profile(models.Model):
             sub.save()
             sub.feed.setup_feed_for_premium_subscribers()
         
+        logging.info(' ---> [%s] NEW PREMIUM ACCOUNT! WOOHOO!!! %s subscriptions!' % (self.user.username, subs.count()))
+        message = """Woohoo!
+        
+User: %(user)s
+Feeds: %(feeds)s
+
+Sincerely,
+NewsBlur""" % {'user': self.user.username, 'feeds': subs.count()}
+        mail_admins('New premium account', message, fail_silently=True)
         
 def create_profile(sender, instance, created, **kwargs):
     if created:
