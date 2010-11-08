3afe36171ea496e91bbf9804f2eb2727f88c1a0e
Samuel Clay <samuel@ofbrooklyn.com>
2010-11-07 22:32:27 -0500

    Story count cutoffs get even more restrictive in the hopes of tirmming feed load times.

--- apps/rss_feeds/models.py
+++ apps/rss_feeds/models.py
@@ -447,13 +447,13 @@ class Feed(models.Model):
         from apps.reader.models import MUserStory
         trim_cutoff = 250
         if self.active_subscribers <= 1:
-            trim_cutoff = 100
+            trim_cutoff = 50
         elif self.active_subscribers <= 3:
-            trim_cutoff = 150
+            trim_cutoff = 100
         elif self.active_subscribers <= 5:
-            trim_cutoff = 200
+            trim_cutoff = 150
         elif self.active_subscribers <= 10:
-            trim_cutoff = 250
+            trim_cutoff = 200
         stories = MStory.objects(
             story_feed_id=self.pk,
         ).order_by('-story_date')
