67bbd4abfb3ffd77cad8e478c65a72ac2e246eed
Samuel Clay <samuel@ofbrooklyn.com>
2010-11-13 19:31:02 -0500

    Fixing munin graphs for feeds. Adding exception_feeds/pages.

--- utils/munin/newsblur_feeds.py
+++ utils/munin/newsblur_feeds.py
@@ -10,6 +10,7 @@ graph_config = {
     'feeds.label': 'feeds',
     'subscriptions.label': 'subscriptions',
     'exception_feeds.label': 'exception_feeds',
+    'exception_pages.label': 'exception_pages',
     'inactive_feeds.label': 'inactive_feeds',
     'duplicate_feeds.label': 'duplicate_feeds',
     'active_feeds.label': 'active_feeds',
@@ -18,7 +19,8 @@ graph_config = {
 metrics = {
     'feeds': Feed.objects.count(),
     'subscriptions': UserSubscription.objects.count(),
-    'exception_feeds': Feed.objects.filter(has_exception=True).count(),
+    'exception_feeds': Feed.objects.filter(has_feed_exception=True).count(),
+    'exception_pages': Feed.objects.filter(has_page_exception=True).count(),
     'inactive_feeds': Feed.objects.filter(active=False).count(),
     'duplicate_feeds': DuplicateFeed.objects.count(),
     'active_feeds': Feed.objects.filter(active_subscribers__gt=0).count(),
