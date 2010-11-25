5702aaf76de3187a574d19dfc5f02f7f76ef9fdf
Samuel Clay <samuel@ofbrooklyn.com>
2010-11-25 09:20:06 -0500

    Fixing a bug when deleting empty folders.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -570,7 +570,8 @@ def delete_folder(request):
     folder_to_delete = request.POST['folder_name']
     in_folder = request.POST.get('in_folder', '')
     feed_ids_in_folder = request.REQUEST.getlist('feed_id')
-    feed_ids_in_folder = [int(f) for f in feed_ids_in_folder]
+    if feed_ids_in_folder:
+        feed_ids_in_folder = [int(f) for f in feed_ids_in_folder]
     
     # Works piss poor with duplicate folder titles, if they are both in the same folder.
     # Deletes all, but only in the same folder parent. But nobody should be doing that, right?
