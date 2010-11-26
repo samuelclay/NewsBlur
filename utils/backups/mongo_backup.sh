61128e00fe300b657fb131f07357b884e7b47bf1
Samuel Clay <samuel@ofbrooklyn.com>
2010-11-26 13:54:26 -0500

    Outputting mongodump command for debugging.

--- utils/backups/mongo_backup.sh
+++ utils/backups/mongo_backup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-MONGODB_SHELL='/usr/bin/mongo'
-DUMP_UTILITY='/usr/bin/mongodump'
+MONGODB_SHELL='mongo'
+DUMP_UTILITY='mongodump'
 DB_NAME='newsblur'
 COLLECTIONS="classifier_tag classifier_author classifier_feed classifier_title"
 
 date_now=`date +%Y_%m_%d_%H_%M`
-dir_name='mongo_backup_'${date_now}
-file_name='mongo_backup_'${date_now}'.bz2'
+dir_name='backup_mongo_'${date_now}
+file_name='backup_mongo_'${date_now}'.bz2'
 
 log() {
     echo $1
@@ -23,7 +23,9 @@ do_backup(){
     # ${MONGODB_SHELL} admin fsync_lock.js
     for collection in $COLLECTIONS
     do
-        ${DUMP_UTILITY} -d ${DB_NAME} -o ${dir_name} -c $collection
+        cmd="${DUMP_UTILITY} -d ${DB_NAME} -o ${dir_name} -c $collection"
+        echo $cmd
+        `$cmd`
     done
     tar -jcf $file_name ${dir_name}
     # ${MONGODB_SHELL} admin fsync_unlock.js
