81e020585976240f48d0bf7204ee9e33bfd258cd
Samuel Clay <samuel@ofbrooklyn.com>
2010-12-31 14:38:19 -0500

    Speeding up feed title arrows.

--- media/js/newsblur/reader.js
+++ media/js/newsblur/reader.js
@@ -1024,7 +1024,7 @@
             $feeds.hover(function() {
                 if (!self.$s.$feed_list.hasClass('NB-feed-sorting')) {
                     var $this = $(this);
-                    $('.NB-hover', $folder).removeClass('NB-hover');
+                    _.defer(function() { $('.NB-hover', $folder).not($this).removeClass('NB-hover'); });
                     $this.addClass("NB-hover");
                     // NEWSBLUR.log(['scroll', $this.scrollTop(), $this.offset(), $this.position()]);
                     if ($this.offset().top > $(window).height() - 270) {
