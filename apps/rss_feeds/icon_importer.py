40b6ac4c7ca61770027be49590003314cf0ffc55
Samuel Clay <samuel@ofbrooklyn.com>
2011-01-30 13:25:45 -0500

    Handling parser errors from lxml in icon fetching.

--- apps/rss_feeds/icon_importer.py
+++ apps/rss_feeds/icon_importer.py
@@ -189,7 +189,7 @@ class IconImporter(object):
             icon_path = lxml.html.fromstring(content).xpath(
                 '//link[@rel="icon" or @rel="shortcut icon"]/@href'
             )
-        except lxml.ParserError:
+        except lxml.etree.ParserError:
             return url
             
         if icon_path:
