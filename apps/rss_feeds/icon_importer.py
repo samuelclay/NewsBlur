70bb450b8d7456fbffee9aec35690d0be97903de
Samuel Clay <samuel@ofbrooklyn.com>
2011-01-30 20:16:34 -0500

    Handling a weird typeerror on feed parsaing.

--- apps/rss_feeds/icon_importer.py
+++ apps/rss_feeds/icon_importer.py
@@ -190,7 +190,7 @@ class IconImporter(object):
             icon_path = lxml.html.fromstring(content).xpath(
                 '//link[@rel="icon" or @rel="shortcut icon"]/@href'
             )
-        except lxml.etree.ParserError:
+        except (lxml.etree.ParserError, TypeError):
             return url
             
         if icon_path:
