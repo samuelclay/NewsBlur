db685f36e2e3decc4529241af819be5a78e44b1a
Samuel Clay <samuel@ofbrooklyn.com>
2011-04-01 15:10:54 -0400

    Adding links to static pages (about, faq, privacy, tos, api, etc).

---
+++ apps/static/views.py
@@ -0,0 +1,34 @@
+import os
+import base64
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from apps.profile.models import Profile
+from apps.reader.models import UserSubscription, UserSubscriptionFolders
+from utils import json_functions as json
+from utils import log as logging
+
+def about(request):
+    return render_to_response('static/about.xhtml', {}, 
+                              context_instance=RequestContext(request))
+                              
+def faq(request):
+    return render_to_response('static/faq.xhtml', {}, 
+                              context_instance=RequestContext(request))
+                              
+def api(request):
+    return render_to_response('static/api.xhtml', {}, 
+                              context_instance=RequestContext(request))
+                              
+def press(request):
+    return render_to_response('static/press.xhtml', {}, 
+                              context_instance=RequestContext(request))
+                              
+def privacy(request):
+    return render_to_response('static/privacy_policy.xhtml', {}, 
+                              context_instance=RequestContext(request))
+                              
+def tos(request):
+    return render_to_response('static/tos.xhtml', {}, 
+                              context_instance=RequestContext(request))
