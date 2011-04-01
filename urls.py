61f750e4b832645780687327aafeb28691bc868b
Samuel Clay <samuel@ofbrooklyn.com>
2011-04-01 15:10:30 -0400

    Making a More More More module. This is ugly as shit.

--- urls.py
+++ urls.py
@@ -1,16 +1,23 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from apps.reader import views as reader_views
+from apps.static import views as static_views
 
 urlpatterns = patterns('',
-    url(r'^$', reader_views.index, name='index'),
-    (r'^reader/', include('apps.reader.urls')),
-    (r'^rss_feeds/', include('apps.rss_feeds.urls')),
-    (r'^classifier/', include('apps.analyzer.urls')),
-    (r'^profile/', include('apps.profile.urls')),
-    (r'^import/', include('apps.feed_import.urls')),
-    (r'^api/', include('apps.api.urls')),
-    (r'^recommendations/', include('apps.recommendations.urls')),
+    url(r'^$',              reader_views.index, name='index'),
+    (r'^reader/',           include('apps.reader.urls')),
+    (r'^rss_feeds/',        include('apps.rss_feeds.urls')),
+    (r'^classifier/',       include('apps.analyzer.urls')),
+    (r'^profile/',          include('apps.profile.urls')),
+    (r'^import/',           include('apps.feed_import.urls')),
+    (r'^api/',              include('apps.api.urls')),
+    (r'^recommendations/',  include('apps.recommendations.urls')),
+    url(r'^about/?',        static_views.about, name='about'),
+    url(r'^faq/?',          static_views.faq, name='faq'),
+    url(r'^api/?',          static_views.api, name='api'),
+    url(r'^press/?',        static_views.press, name='press'),
+    url(r'^privacy/?',      static_views.privacy, name='privacy'),
+    url(r'^tos/?',          static_views.tos, name='tos'),
 )
 
 if settings.DEVELOPMENT:
