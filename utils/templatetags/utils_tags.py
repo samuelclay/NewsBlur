cb125b8dd4198f6a500aa31ba7e33f62b871dc5d
Samuel Clay <samuel@ofbrooklyn.com>
2011-04-15 09:48:07 -0400

    Adding graphs to stats on dashboard. Still need to collect stats.

--- utils/templatetags/utils_tags.py
+++ utils/templatetags/utils_tags.py
@@ -6,3 +6,24 @@ register = template.Library()
 @register.simple_tag
 def current_domain():
     return Site.objects.get_current().domain
+    
+@register.filter
+def get_range( value ):
+  """
+    Filter - returns a list containing range made from given value
+    Usage (in template):
+
+    <ul>{% for i in 3|get_range %}
+      <li>{{ i }}. Do something</li>
+    {% endfor %}</ul>
+
+    Results with the HTML:
+    <ul>
+      <li>0. Do something</li>
+      <li>1. Do something</li>
+      <li>2. Do something</li>
+    </ul>
+
+    Instead of 3 one may use the variable set in the views
+  """
+  return range( value )
