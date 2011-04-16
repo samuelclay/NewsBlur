341265447c7b52ac55bd125167e3b6702bb7fcea
Samuel Clay <samuel@ofbrooklyn.com>
2011-04-16 16:24:33 -0400

    Divide by float!

--- apps/statistics/templatetags/statistics_tags.py
+++ apps/statistics/templatetags/statistics_tags.py
@@ -6,4 +6,4 @@ register = template.Library()
 def format_graph(n, max_value, height=30):
     if n == 0 or max_value == 0:
         return 1
-    return max(1, height / (n/max_value))
+    return max(1, height / (n/float(max_value)))
