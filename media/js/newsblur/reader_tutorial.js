0bf05d11b51150d1c859802196f969664b4b13dd
Samuel Clay <samuel@ofbrooklyn.com>
2011-05-11 20:18:33 -0400

    Finishing paging. Now just need to add the page layouts and hiding preference.

--- media/js/newsblur/reader_tutorial.js
+++ media/js/newsblur/reader_tutorial.js
@@ -20,6 +20,7 @@ _.extend(NEWSBLUR.ReaderTutorial.prototype, {
     runner: function() {
         this.make_modal();
         this.open_modal();
+        this.page(1);
         
         this.$modal.bind('click', $.rescope(this.handle_click, this));
     },
@@ -29,6 +30,7 @@ _.extend(NEWSBLUR.ReaderTutorial.prototype, {
         
         this.$modal = $.make('div', { className: 'NB-modal-tutorial NB-modal' }, [
             $.make('span', { className: 'NB-modal-loading NB-spinner'}),
+            $.make('div', { className: 'NB-modal-page' }),
             $.make('h2', { className: 'NB-modal-title' }, 'Learn to use NewsBlur'),
             $.make('div', { className: 'NB-page NB-page-1' }, [
               'Page 1'
@@ -69,15 +71,21 @@ _.extend(NEWSBLUR.ReaderTutorial.prototype, {
       this.page_number = page_number;
       
       if (page_number == page_count) {
-        $('.NB-tutorial-next-page-text', this.$modal).text('Finish Tutorial');
+        $('.NB-tutorial-next-page-text', this.$modal).text('Finish Tutorial ');
       } else if (page_number > page_count) {
         return this.close();
       } else {
-        $('.NB-tutorial-next-page-text', this.$modal).text('Next Page');
-        
+        $('.NB-tutorial-next-page-text', this.$modal).text('Next Page ');
       }
+      $('.NB-page-previous', this.$modal).toggle(page_number != 1);
       $('.NB-page', this.$modal).css({'display': 'none'});
-      $('.NB-page-'+this.page_number, this.$modal).css({'display': 'none'});
+      $('.NB-page-'+this.page_number, this.$modal).css({'display': 'block'});
+      $('.NB-modal-page', this.$modal).html($.make('div', [
+        'Page ',
+        $.make('b', this.page_number),
+        ' of ',
+        $.make('b', page_count)
+      ]));
     },
     
     // ===========
