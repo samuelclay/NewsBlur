a909c675b8fbb67b622f25203eeccceadc85eb1d
Samuel Clay <samuel@ofbrooklyn.com>
2011-05-18 14:59:05 -0400

    Cleaning up mongo backup.

--- utils/backups/backup_mongo.py
+++ utils/backups/backup_mongo.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 CURRENT_DIR  = os.path.dirname(__file__)
 NEWSBLUR_DIR = ''.join([CURRENT_DIR, '/../../'])
@@ -13,16 +14,17 @@ from django.conf import settings
 COLLECTIONS = "classifier_tag classifier_author classifier_feed classifier_title userstories starred_stories"
 
 date = time.strftime('%Y-%m-%d-%H-%M')
-collections = ' '.split(COLLECTIONS)
+collections = COLLECTIONS.split(' ')
 db_name = settings.MONGO_DB.get('NAME')
 db_pass = settings.MONGO_DB.get('PASSWORD')
 dir_name = 'backup_mongo_%s' % date
 filename = '%s.tgz' % dir_name
 
+os.mkdir(dir_name)
+
 for collection in collections:
-    print "Dumping %s" % collection
-    filename = 'backup_mongo_%s.sql.gz' % date
     cmd = 'mongodump  --db %s --collection %s -o %s' % (db_name, collection, dir_name)
+    print "Dumping %s: %s" % (collection, cmd)
     os.system(cmd)
     
 cmd = 'tar -jcf %s %s' % (filename, dir_name)
@@ -30,4 +32,5 @@ os.system(cmd)
 
 print 'Uploading %s to S3...' % filename
 s3.save_file_in_s3(filename)
-os.removedirs(dir_name)
+shutil.rmtree(dir_name)
+os.remove(filename)
