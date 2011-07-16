792afc1346e21fbb38e7821fd1af8c0a442b6f2d
Samuel Clay <samuel@ofbrooklyn.com>
2011-07-15 17:51:22 -0700

    Adding back/forward buttons and a url and title bar to original view in iphone app.

--- media/iphone/Classes/OriginalStoryViewController.h
+++ media/iphone/Classes/OriginalStoryViewController.h
@@ -10,6 +10,13 @@
 
 @class NewsBlurAppDelegate;
 
+static const CGFloat kNavBarHeight  = 52.0f;
+static const CGFloat kLabelHeight   = 18.0f;
+static const CGFloat kMargin        = 10.0f;
+static const CGFloat kSpacer        = 2.0f;
+static const CGFloat kLabelFontSize = 12.0f;
+static const CGFloat kAddressHeight = 22.0f;
+
 @interface OriginalStoryViewController : UIViewController {
     
     NewsBlurAppDelegate *appDelegate;
@@ -21,6 +28,8 @@
     UIBarButtonItem* forward;
     UIBarButtonItem* refresh;
     UIBarButtonItem* pageAction;
+    UILabel *pageTitle;
+    UITextField *pageUrl;
 }
 
 @property (nonatomic, retain) IBOutlet NewsBlurAppDelegate *appDelegate;
@@ -30,9 +39,14 @@
 @property (nonatomic, retain) IBOutlet UIBarButtonItem* forward;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem* refresh;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem* pageAction;
+@property (nonatomic, retain) IBOutlet UILabel *pageTitle;
+@property (nonatomic, retain) IBOutlet UITextField *pageUrl;
 
 - (IBAction) doCloseOriginalStoryViewController;
-
+- (void)loadAddress:(id)sender event:(UIEvent*)event;
+- (void)updateTitle:(UIWebView*)aWebView;
+- (void)updateAddress:(NSURLRequest*)request;
 - (void)updateButtons;
+- (void)informError:(NSError*)error;    
 
 @end
--- media/iphone/Classes/OriginalStoryViewController.m
+++ media/iphone/Classes/OriginalStoryViewController.m
@@ -19,6 +19,8 @@
 @synthesize forward;
 @synthesize refresh;
 @synthesize pageAction;
+@synthesize pageTitle;
+@synthesize pageUrl;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
 	
@@ -32,6 +34,130 @@
     [appDelegate showNavigationBar:NO];
     NSURLRequest *request = [[[NSURLRequest alloc] initWithURL:[appDelegate activeOriginalStoryURL]] autorelease];
     [webView loadRequest:request];
+}
+
+- (void)viewDidLoad {
+    
+    CGRect navBarFrame = self.view.bounds;
+    navBarFrame.size.height = kNavBarHeight;
+    UINavigationBar *navBar = [[UINavigationBar alloc] initWithFrame:navBarFrame];
+    navBar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    
+    CGRect labelFrame = CGRectMake(kMargin, kSpacer,
+                                   navBar.bounds.size.width - 2*kMargin, 
+                                   kLabelHeight);
+    UILabel *label = [[UILabel alloc] initWithFrame:labelFrame];
+    label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    label.backgroundColor = [UIColor clearColor];
+    label.font = [UIFont systemFontOfSize:12];
+    label.textAlignment = UITextAlignmentCenter;
+    label.text = [[appDelegate activeStory] objectForKey:@"story_title"];
+    [navBar addSubview:label];
+    self.pageTitle = label;
+    [label release];
+    
+    CGRect addressFrame = CGRectMake(kMargin, kSpacer*2.0 + kLabelHeight,
+                                     labelFrame.size.width - 60, kAddressHeight);
+    UITextField *address = [[UITextField alloc] initWithFrame:addressFrame];
+    address.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    address.borderStyle = UITextBorderStyleRoundedRect;
+    address.font = [UIFont systemFontOfSize:15];
+    address.keyboardType = UIKeyboardTypeURL;
+    address.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    address.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [navBar addSubview:address];
+    self.pageUrl = address;
+    
+    [self.view addSubview:navBar];
+    
+    CGRect webViewFrame = self.webView.frame;
+    webViewFrame.origin.y = navBarFrame.origin.y + navBarFrame.size.height;
+//    webViewFrame.size.height = self.toolbar.frame.origin.y - webViewFrame.origin.y;
+    self.webView.frame = webViewFrame;
+    
+    CGRect closeButtonFrame = CGRectMake(addressFrame.size.width + kMargin*2, 
+                                         addressFrame.origin.y, 48,
+                                         addressFrame.size.height);
+    UIButton *close = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [close setFrame:closeButtonFrame];
+    [close setFont:[UIFont systemFontOfSize:12]];
+    [close setTitle:@"Close" forState:UIControlStateNormal];
+    [close addTarget:self action:@selector(doCloseOriginalStoryViewController) forControlEvents:UIControlEventTouchUpInside];
+    [navBar addSubview:close];
+    
+    [navBar release];
+    [address release];
+
+}
+
+- (void)loadAddress:(id)sender event:(UIEvent *)event
+{
+    NSString* urlString = self.pageUrl.text;
+    NSURL* url = [NSURL URLWithString:urlString];
+    
+    if (!url.scheme) {
+        NSString* modifiedURLString = [NSString stringWithFormat:@"http://%@", urlString];
+        url = [NSURL URLWithString:modifiedURLString];
+    }
+    
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+
+// MARK: -
+// MARK: UIWebViewDelegate protocol
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    [self updateAddress:request];
+    return YES;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)aWebView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    [self updateButtons];
+}
+- (void)webViewDidFinishLoad:(UIWebView *)aWebView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self updateButtons];
+    [self updateTitle:aWebView];
+    NSURLRequest* request = [aWebView request];
+    [self updateAddress:request];
+}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self updateButtons];
+    [self informError:error];
+}
+- (void)updateTitle:(UIWebView*)aWebView
+{
+    NSString* pageTitle = [aWebView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    self.pageTitle.text = pageTitle;
+}
+- (void)updateAddress:(NSURLRequest*)request
+{
+    NSURL* url = [request mainDocumentURL];
+    NSString* absoluteString = [url absoluteString];
+    self.pageUrl.text = absoluteString;
+}
+- (void)updateButtons
+{
+    self.forward.enabled = self.webView.canGoForward;
+    self.back.enabled = self.webView.canGoBack;
+//    self.stop.enabled = self.webView.loading;
+}
+- (void)informError:(NSError *)error
+{
+    NSString* localizedDescription = [error localizedDescription];
+    UIAlertView* alertView = [[UIAlertView alloc]
+                              initWithTitle:@"Error"
+                              message:localizedDescription delegate:nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+    [alertView show];
+    [alertView release];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -50,6 +176,8 @@
     self.forward = nil;
     self.refresh = nil;
     self.pageAction = nil;
+    self.pageTitle = nil;
+    self.pageUrl = nil;
 }
 
 
@@ -61,6 +189,8 @@
     [forward release];
     [refresh release];
     [pageAction release];
+    [pageTitle release];
+    [pageUrl release];
 }
 
 
