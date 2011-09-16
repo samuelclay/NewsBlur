6417c28c419b1cf98aa2d83380dd3d5b44e52d5c
Samuel Clay <samuel@ofbrooklyn.com>
2011-09-15 18:40:27 -0700

    Fixing sizing on iphone app's original view's url.

--- media/iphone/Classes/OriginalStoryViewController.m
+++ media/iphone/Classes/OriginalStoryViewController.m
@@ -104,7 +104,8 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
     UITextField *address = [[UITextField alloc] initWithFrame:addressFrame];
     address.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     address.borderStyle = UITextBorderStyleRoundedRect;
-    address.font = [UIFont systemFontOfSize:18];
+    address.font = [UIFont systemFontOfSize:14];
+    address.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     address.adjustsFontSizeToFitWidth = NO;
     address.keyboardType = UIKeyboardTypeURL;
     address.autocapitalizationType = UITextAutocapitalizationTypeNone;
