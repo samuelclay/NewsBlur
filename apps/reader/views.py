408b8e16e963b6dc09b59f87b5ed3dafa67c6951
Samuel Clay <samuel@ofbrooklyn.com>
2011-09-15 18:22:03 -0700

    Showing correct story_id in logging for bad read stories.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -653,7 +653,7 @@ def mark_story_as_read(request):
             logging.user(request.user, "~BRSubscription mark_read_date: %s, oldest_unread_story_date: %s" % (
                 usersub.mark_read_date, usersub.oldest_unread_story_date))
             m = MUserStory.objects.get(story=story, user_id=request.user.pk, feed_id=feed_id)
-            logging.user(request.user, "~BROriginal read date: %s, story id: %s" % (m.read_date, m.story_guid))
+            logging.user(request.user, "~BROriginal read date: %s, story id: %s" % (m.read_date, m.story_id))
             m.read_date = date
             m.save()
     
