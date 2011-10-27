b98d046aa191b9d5324629a37a7805197a68e8a2
Samuel Clay <samuel@ofbrooklyn.com>
2011-10-26 20:31:44 -0700

    Adding vendorized seacucumber, for when I hate amazon ses.

---
+++ vendor/seacucumber/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sea Cucumber is a Django email backend for Amazon Simple Email Service, 
+backed by celery. The interesting bits are in backend.py and tasks.py. The
+rest of the contents of this module are largely optional. 
+"""
+# In the form of Major, Minor.
+VERSION = (1, 3)
