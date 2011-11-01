bd5bc842275e7fa09000d5f39d5d69d3d3654132
Samuel Clay <samuel@ofbrooklyn.com>
2011-11-01 07:44:09 -0700

    Temporarily slowing donw feed fetcher.

--- apps/rss_feeds/models.py
+++ apps/rss_feeds/models.py
@@ -894,7 +894,7 @@ class Feed(models.Model):
         # 2 subscribers:
         #   1 update per day = 1 hours
         #   10 updates = 20 minutes
-        updates_per_day_delay = 12 * 60 / max(.25, ((max(0, self.active_subscribers)**.15)
+        updates_per_day_delay = 12 * 60 / max(.25, ((max(0, self.active_subscribers)**.35)
                                                     * (updates_per_month**1.2)))
         if self.premium_subscribers > 0:
             updates_per_day_delay /= min(self.active_subscribers+self.premium_subscribers, 5)
