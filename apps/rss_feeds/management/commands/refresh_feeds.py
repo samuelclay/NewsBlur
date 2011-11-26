7972d0bb5c4f9dac61064bb05fe6586c37820061
Samuel Clay <samuel@ofbrooklyn.com>
2011-11-26 02:16:14 -0500

    Revert "Switching to requests from urllib2/httplib. Bring on the page errors."
    This reverts commit 617dc3bb56c6962387cc8d2a7501beb63e8fab29.

--- apps/rss_feeds/management/commands/refresh_feeds.py
+++ apps/rss_feeds/management/commands/refresh_feeds.py
@@ -5,7 +5,6 @@ from apps.rss_feeds.models import Feed
 from optparse import make_option
 from utils import feed_fetcher
 from utils.management_functions import daemonize
-import django
 import socket
 import datetime
 import redis
@@ -76,14 +75,12 @@ class Command(BaseCommand):
         feeds_queue = []
         for _ in range(num_workers):
             feeds_queue.append([])
-        
+            
         i = 0
         for feed in feeds:
             feeds_queue[i%num_workers].append(feed.pk)
             i += 1
         disp.add_jobs(feeds_queue, i)
-        
-        django.db.connection.close()
         
         print " ---> Fetching %s feeds..." % feeds.count()
         disp.run_jobs()
