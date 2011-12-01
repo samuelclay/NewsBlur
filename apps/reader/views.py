6ee40269636a2d92b16052bc1d16b2b376be3368
Samuel Clay <samuel@ofbrooklyn.com>
2011-11-30 21:29:31 -0800

    Inverted logic on favicons fetching. Whoops. Fix in < 60 seconds, though.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -304,7 +304,7 @@ def refresh_feeds(request):
         if sub.feed.pk in favicons_fetching and sub.feed.pk in feed_icons:
             feeds[pk]['favicon'] = feed_icons[sub.feed.pk].data
             feeds[pk]['favicon_color'] = feed_icons[sub.feed.pk].color
-            feeds[pk]['favicon_fetching'] = bool(not (sub.feed.favicon_fetching))
+            feeds[pk]['favicon_fetching'] = sub.feed.favicon_fetching
     
     user_subs = UserSubscription.objects.select_related('feed').filter(user=user, active=True)
     
