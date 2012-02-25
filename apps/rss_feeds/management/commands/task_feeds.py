cbc8df2c1c907e65bcd687ac8e4fb80dd3bed3f0
Samuel Clay <samuel@ofbrooklyn.com>
2012-02-24 17:07:42 -0800

    Skipping feed resurrection.

--- apps/rss_feeds/management/commands/task_feeds.py
+++ apps/rss_feeds/management/commands/task_feeds.py
@@ -48,10 +48,10 @@ class Command(BaseCommand):
         ).order_by('?')
         if feeds: Feed.task_feeds(feeds)
         
-        feeds = Feed.objects.filter(
-            last_update__lte=day,
-            active_subscribers__gte=1,
-            active=False,
-            known_good=True
-        ).order_by('?')
-        if feeds: Feed.task_feeds(feeds)
+        # feeds = Feed.objects.filter(
+        #     last_update__lte=day,
+        #     active_subscribers__gte=1,
+        #     active=False,
+        #     known_good=True
+        # ).order_by('?')
+        # if feeds: Feed.task_feeds(feeds)
