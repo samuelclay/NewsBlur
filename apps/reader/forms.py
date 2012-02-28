936088f09c6f2d746536118b6f8b4ad06f55de9d
Samuel Clay <samuel@ofbrooklyn.com>
2012-02-27 16:36:39 -0800

    Allowing any password to be used on accounts with no password set.

--- apps/reader/forms.py
+++ apps/reader/forms.py
@@ -28,24 +28,28 @@ class LoginForm(forms.Form):
         if username and user:
             self.user_cache = authenticate(username=user[0].username, password=password)
             if self.user_cache is None:
+                self.user_cache = authenticate(username=user[0].username, password="")
+            if self.user_cache is None:
                 email_username = User.objects.filter(email=username)
                 if email_username:
                     self.user_cache = authenticate(username=email_username[0].username, password=password)
+                    if self.username is None:
+                        self.user_cache = authenticate(username=email_username[0].username, password="")
+            if self.user_cache is None:
+                # logging.info(" ***> [%s] Bad Login: TRYING JK-LESS PASSWORD" % username)
+                jkless_password = password.replace('j', '').replace('k', '')
+                self.user_cache = authenticate(username=username, password=jkless_password)
                 if self.user_cache is None:
-                    # logging.info(" ***> [%s] Bad Login: TRYING JK-LESS PASSWORD" % username)
-                    jkless_password = password.replace('j', '').replace('k', '')
-                    self.user_cache = authenticate(username=username, password=jkless_password)
-                    if self.user_cache is None:
-                        logging.info(" ***> [%s] Bad Login" % username)
-                        raise forms.ValidationError(_("Whoopsy-daisy. Try again."))
-                    else:
-                        # Supreme fuck-up. Accidentally removed the letters J and K from
-                        # all user passwords. Re-save with correct password.
-                        logging.info(" ***> [%s] FIXING JK-LESS PASSWORD" % username)
-                        self.user_cache.set_password(password)
-                        self.user_cache.save()
-                elif not self.user_cache.is_active:
-                    raise forms.ValidationError(_("This account is inactive."))
+                    logging.info(" ***> [%s] Bad Login" % username)
+                    raise forms.ValidationError(_("Whoopsy-daisy. Try again."))
+                else:
+                    # Supreme fuck-up. Accidentally removed the letters J and K from
+                    # all user passwords. Re-save with correct password.
+                    logging.info(" ***> [%s] FIXING JK-LESS PASSWORD" % username)
+                    self.user_cache.set_password(password)
+                    self.user_cache.save()
+            if not self.user_cache.is_active:
+                raise forms.ValidationError(_("This account is inactive."))
         elif username and not user:
             raise forms.ValidationError(_("That username is not registered. Create an account with it instead."))
             
