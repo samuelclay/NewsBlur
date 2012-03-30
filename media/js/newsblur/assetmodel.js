f0e9e25a1b3052ba5fda7cb024b18700dd849f51
Samuel Clay <samuel@ofbrooklyn.com>
2012-03-30 16:03:07 -0700

    Launching real-time for premiums. Also fixing a few dupe feed refresh bugs.

--- media/js/newsblur/assetmodel.js
+++ media/js/newsblur/assetmodel.js
@@ -470,14 +470,16 @@ NEWSBLUR.AssetModel.Reader.prototype = {
     
     post_refresh_feeds: function(data, callback) {
         var updated_feeds = [];
-
+        
+        if (!data.feeds) return;
+        
         for (var f in data.feeds) {
             if (!this.feeds[f]) continue;
             var updated = false;
             f = parseInt(f, 10);
             var feed = data.feeds[f];
             if (feed.id && f != feed.id) {
-                NEWSBLUR.log(['Dupe feed being refreshed', f, feed.id, this.feeds[f]]);
+                NEWSBLUR.log(['Dupe feed being refreshed', f, feed.id, this.feeds[f], feed]);
                 this.feeds[feed.id] = this.feeds[f];
             }
             if ((feed['has_exception'] && !this.feeds[f]['has_exception']) ||
@@ -493,7 +495,7 @@ NEWSBLUR.AssetModel.Reader.prototype = {
                     updated = true;
                 }
             }
-            if (feed['favicon']) {
+            if (feed['favicon'] && this.feeds[f]['favicon'] != feed['favicon']) {
                 this.feeds[f]['favicon'] = feed['favicon'];
                 this.feeds[f]['favicon_color'] = feed['favicon_color'];
                 this.feeds[f]['favicon_fetching'] = false;
@@ -540,10 +542,10 @@ NEWSBLUR.AssetModel.Reader.prototype = {
             var feed = this.feeds[f];
             
             if (feed.active) {
-                if (feed['not_yet_fetched']) {
-                    counts['unfetched_feeds'] += 1;
-                } else {
+                if (!feed['not_yet_fetched'] || feed['has_exception']) {
                     counts['fetched_feeds'] += 1;
+                } else {
+                    counts['unfetched_feeds'] += 1;
                 }
             }
         }
