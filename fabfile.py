5f167251bbdfc4b1268a5051ab62008b153f1437
Samuel Clay <samuel@ofbrooklyn.com>
2012-05-03 18:33:29 -0700

    Adding backup restore for psql.

--- fabfile.py
+++ fabfile.py
@@ -545,17 +545,19 @@ def setup_mongo():
     sudo('apt-get -y install mongodb-10gen')
 
 def setup_redis():
+    redis_version = '2.4.13'
     with cd(env.VENDOR_PATH):
-        run('wget http://redis.googlecode.com/files/redis-2.4.2.tar.gz')
-        run('tar -xzf redis-2.4.2.tar.gz')
-        run('rm redis-2.4.2.tar.gz')
-    with cd(os.path.join(env.VENDOR_PATH, 'redis-2.4.2')):
+        run('wget http://redis.googlecode.com/files/redis-%s.tar.gz' % redis_version)
+        run('tar -xzf redis-%s.tar.gz' % redis_version)
+        run('rm redis-%s.tar.gz' % redis_version)
+    with cd(os.path.join(env.VENDOR_PATH, 'redis-%s' % redis_version)):
         sudo('make install')
     put('config/redis-init', '/etc/init.d/redis', use_sudo=True)
     sudo('chmod u+x /etc/init.d/redis')
     put('config/redis.conf', '/etc/redis.conf', use_sudo=True)
     sudo('mkdir -p /var/lib/redis')
     sudo('update-rc.d redis defaults')
+    sudo('/etc/init.d/redis stop')
     sudo('/etc/init.d/redis start')
 
 def setup_db_munin():
@@ -582,6 +584,19 @@ def copy_task_settings():
     put('config/settings/task_settings.py', '%s/local_settings.py' % env.NEWSBLUR_PATH)
     run('echo "\nSERVER_NAME = \\\\"`hostname`\\\\"" >> %s/local_settings.py' % env.NEWSBLUR_PATH)
 
+
+# ==============
+# = Tasks - DB =
+# ==============
+
+def restore_postgres():
+    backup_date = '2012-05-03-08-00'
+    run('PYTHONPATH=/home/sclay/newsblur python s3.py get backup_postgresql_%s.sql.gz' % backup_date)
+    sudo('su postgres -c "createuser -U newsblur"')
+    sudo('su postgres -c "createdb newsblur -O newsblur"')
+    sudo('su postgres -c "pg_restore --role=newsblur --dbname=newsblur backup_postgresql_%s.sql.gz"' % backup_date)
+    
+    
 # ======
 # = S3 =
 # ======
