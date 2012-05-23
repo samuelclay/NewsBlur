f07445fc3869b193c32163a16e819e9ad95033cb
Samuel Clay <samuel@ofbrooklyn.com>
2012-05-23 14:29:05 -0700

    Feeds above folders, grouping by type.

--- media/js/newsblur/models/folders.js
+++ media/js/newsblur/models/folders.js
@@ -40,36 +40,25 @@ NEWSBLUR.Collections.Folders = Backbone.Collection.extend({
     
     comparator: function(modelA, modelB) {
         var sort_order = NEWSBLUR.assets.preference('feed_order');
-        var feedA, feedB;
-        if (modelA && modelA.e instanceof jQuery) feedA = NEWSBLUR.assets.feeds.get(parseInt(a.e.data('id'), 10));
-        if (modelB && modelB.e instanceof jQuery) feedB = NEWSBLUR.assets.feeds.get(parseInt(b.e.data('id'), 10));
-        if (modelA && modelA.is_feed()) feedA = modelA.model;
-        if (modelB && modelB.is_feed()) feedB = modelB.model;
         
-        // console.log(["feeds", sort_order, modelA, modelB, feedA, feedB]);
         
+        if (modelA.is_feed() != modelB.is_feed()) {
+            // Feeds above folders
+            return modelA.is_feed() ? -1 : 1;
+        }
+        if (modelA.is_folder()) {
+            // Folders are alphabetical
+            return modelA.get('folder_title').toLowerCase() > modelB.get('folder_title').toLowerCase() ? 1 : -1;
+        }
+        
+        var feedA = modelA.feed;
+        var feedB = modelB.feed;
         if (sort_order == 'ALPHABETICAL' || !sort_order) {
-            if (feedA && feedB) {
-                return feedA.get('feed_title').toLowerCase() > feedB.get('feed_title').toLowerCase() ? 1 : -1;
-            } else if (feedA && !feedB) {
-                return -1;
-            } else if (!feedA && feedB) {
-                return 1;
-            } else if (!feedA && !feedB && modelA && modelB && !modelA.is_feed() && !modelB.is_feed() && !(modelA.e instanceof jQuery) && !(modelB.e instanceof jQuery)) {
-                return modelA.get('folder_title').toLowerCase() > modelB.get('folder_title').toLowerCase() ? 1 : -1;
-            }
+            return feedA.get('feed_title').toLowerCase() > feedB.get('feed_title').toLowerCase() ? 1 : -1;
         } else if (sort_order == 'MOSTUSED') {
-            if (feedA && feedB) {
-                return feedA.get('feed_opens') < feedB.get('feed_opens') ? 1 : 
-                    (feedA.get('feed_opens') > feedB.get('feed_opens') ? -1 : 
-                    (feedA.get('feed_title').toLowerCase() > feedB.get('feed_title').toLowerCase() ? 1 : -1));
-            } else if (feedA && !feedB) {
-                return -1;
-            } else if (!feedA && feedB) {
-                return 1;
-            } else if (!feedA && !feedB && modelA && modelB && !modelA.is_feed() && !modelB.is_feed() && !(modelA.e instanceof jQuery) && !(modelB.e instanceof jQuery)) {
-                return modelA.get('folder_title').toLowerCase() > modelB.get('folder_title').toLowerCase() ? 1 : -1;
-            }
+            return feedA.get('feed_opens') < feedB.get('feed_opens') ? 1 : 
+                (feedA.get('feed_opens') > feedB.get('feed_opens') ? -1 : 
+                (feedA.get('feed_title').toLowerCase() > feedB.get('feed_title').toLowerCase() ? 1 : -1));
         }
     }
 
