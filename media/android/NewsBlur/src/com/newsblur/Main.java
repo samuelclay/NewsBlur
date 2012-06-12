f57be00805968bdcef1c1db85a83207376bbd339
Ryan Bateman <ryan.bateman@gmail.com>
2012-06-11 20:46:58 -0400

    Added projects for NewsBlur Android app and testing.

---
+++ media/android/NewsBlur/src/com/newsblur/Main.java
@@ -0,0 +1,13 @@
+package com.newsblur;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
