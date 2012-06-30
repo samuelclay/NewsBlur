d7f565ea1cacdf52ad9a6d6ba72d61ed5156a323
Samuel Clay <samuel@ofbrooklyn.com>
2012-06-29 23:14:02 -0700

    Adding relative user id to public_comments. If not specified, use current user.

--- media/js/newsblur/social_page/social_page_comments.js
+++ media/js/newsblur/social_page/social_page_comments.js
@@ -9,13 +9,13 @@ NEWSBLUR.Views.SocialPageComments = Backbone.View.extend({
     },
     
     load_public_story_comments: function() {
-        
         $.get('/social/public_comments', {
             story_id: this.options.story_id,
             feed_id: this.options.feed_id,
+            user_id: NEWSBLUR.Globals.blurblog_user_id,
             format: "html"
         }, _.bind(function(template) {
-            var $template = $(template);
+            var $template = $($.trim(template));
             var $header = this.make('div', { 
                 "class": 'NB-story-comments-public-header-wrapper' 
             }, this.make('div', { 
