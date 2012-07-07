8cd0a1da133f4129f51aad4b62537638324d83a3
Samuel Clay <samuel@ofbrooklyn.com>
2012-07-07 12:25:02 -0700

    iOS 6 support. Still needs more, though. But this gets the app running.

--- media/iphone/main.m
+++ media/iphone/main.m
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "NewsBlurAppDelegate.h"
 
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([NewsBlurAppDelegate class]));
     [pool release];
     return retVal;
 }
