f5c9a72c050c760b47a38d2e1104d6448664b7a3
Samuel Clay <samuel@ofbrooklyn.com>
2012-07-17 14:14:45 -0700

    Fixing shift+a for social subs. Also fixing bug where site would stay selected after adding a new site.

--- media/js/newsblur/reader/reader.js
+++ media/js/newsblur/reader/reader.js
@@ -5009,7 +5009,9 @@
             });
             $document.bind('keydown', 'shift+a', function(e) {
                 e.preventDefault();
-                if (self.flags.river_view) {
+                if (self.flags.social_view) {
+                    self.mark_feed_as_read();
+                } else if (self.flags.river_view) {
                     self.mark_folder_as_read();
                 } else {
                     self.mark_feed_as_read();
