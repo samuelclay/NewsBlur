f0dc76b71e1c8095c2b828a54f9b00e5eccd938d
Samuel Clay <samuel@ofbrooklyn.com>
2012-08-09 21:18:45 -0700

    Merging separate task feeds tasks.

--- apps/rss_feeds/management/commands/task_feeds.py
+++ apps/rss_feeds/management/commands/task_feeds.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from optparse import make_option
+from apps.rss_feeds.tasks import TaskFeeds
 import datetime
 
 
@@ -13,43 +14,4 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        from apps.rss_feeds.models import Feed
-        settings.LOG_TO_STREAM = True
-        now = datetime.datetime.utcnow()
-        
-        # Active feeds
-        feeds = Feed.objects.filter(
-            next_scheduled_update__lte=now,
-            active=True
-        ).exclude(
-            active_subscribers=0
-        ).order_by('?')
-        if options['all']:
-            feeds = Feed.objects.all()
-        Feed.task_feeds(feeds)
-        
-        # Mistakenly inactive feeds
-        day = now - datetime.timedelta(days=1)
-        feeds = Feed.objects.filter(
-            last_update__lte=day, 
-            queued_date__lte=day,
-            min_to_decay__lte=60*24,
-            active_subscribers__gte=1
-        ).order_by('?')[:50]
-        if feeds: Feed.task_feeds(feeds)
-        
-        week = now - datetime.timedelta(days=7)
-        feeds = Feed.objects.filter(
-            last_update__lte=week, 
-            queued_date__lte=day,
-            active_subscribers__gte=1
-        ).order_by('?')[:50]
-        if feeds: Feed.task_feeds(feeds)
-        
-        # feeds = Feed.objects.filter(
-        #     last_update__lte=day,
-        #     active_subscribers__gte=1,
-        #     active=False,
-        #     known_good=True
-        # ).order_by('?')
-        # if feeds: Feed.task_feeds(feeds)
+        TaskFeeds.apply()
