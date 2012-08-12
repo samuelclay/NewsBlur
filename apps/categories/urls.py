8820b9a62c22ee7ca6a187b7fc59b33856288d45
Samuel Clay <samuel@ofbrooklyn.com>
2012-08-12 16:49:14 -0700

    Adding categories. Still needs subscription to categories being added to a user's subscription folders.

---
+++ apps/categories/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import url, patterns
+from apps.categories import views
+
+urlpatterns = patterns('',
+    url(r'^/?$', views.all_categories, name='all-categories'),
+    url(r'^subscribe/?$', views.subscribe, name='categories-subscribe'),
+)
