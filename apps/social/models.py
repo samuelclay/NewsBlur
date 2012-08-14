99976d6f1825029cfaca5714a89e6172776d08d0
Samuel Clay <samuel@ofbrooklyn.com>
2012-08-14 00:26:42 -0700

    Probably a bit too much in one commit: allowing categories to be specified in interactions and activities calls so the iPad won't get category types it doesn't know how to handle. Also added twitter/facebook buttons to comment share on blurblogs.

--- apps/social/models.py
+++ apps/social/models.py
@@ -1658,11 +1658,15 @@ class MSocialServices(mongo.Document):
         }
     
     @classmethod
+    def get_user(cls, user_id):
+        profile, created = cls.objects.get_or_create(user_id=user_id)
+        if created:
+            profile.save()
+        return profile
+        
+    @classmethod
     def profile(cls, user_id):
-        try:
-            profile = cls.objects.get(user_id=user_id)
-        except cls.DoesNotExist:
-            return {}
+        profile = cls.get_user(user_id=user_id)
         return profile.to_json()
 
     def twitter_api(self):
@@ -1898,13 +1902,18 @@ class MInteraction(mongo.Document):
         }
         
     @classmethod
-    def user(cls, user_id, page=1, limit=None):
+    def user(cls, user_id, page=1, limit=None, categories=None):
         user_profile = Profile.objects.get(user=user_id)
         dashboard_date = user_profile.dashboard_date or user_profile.last_seen_on
         page = max(1, page)
         limit = int(limit) if limit else 4
         offset = (page-1) * limit
-        interactions_db = cls.objects.filter(user_id=user_id)[offset:offset+limit+1]
+        
+        interactions_db = cls.objects.filter(user_id=user_id)
+        if categories:
+            interactions_db = interactions_db.filter(category__in=categories)
+        interactions_db = interactions_db[offset:offset+limit+1]
+        
         has_next_page = len(interactions_db) > limit
         interactions_db = interactions_db[offset:offset+limit]
         with_user_ids = [i.with_user_id for i in interactions_db if i.with_user_id]
@@ -2090,7 +2099,7 @@ class MActivity(mongo.Document):
         }
         
     @classmethod
-    def user(cls, user_id, page=1, limit=4, public=False):
+    def user(cls, user_id, page=1, limit=4, public=False, categories=None):
         user_profile = Profile.objects.get(user=user_id)
         dashboard_date = user_profile.dashboard_date or user_profile.last_seen_on
         page = max(1, page)
@@ -2098,10 +2107,12 @@ class MActivity(mongo.Document):
         offset = (page-1) * limit
         
         activities_db = cls.objects.filter(user_id=user_id)
+        if categories:
+            activities_db = activities_db.filter(category__in=categories)
         if public:
             activities_db = activities_db.filter(category__nin=['star', 'feedsub'])
-            
         activities_db = activities_db[offset:offset+limit+1]
+        
         has_next_page = len(activities_db) > limit
         activities_db = activities_db[offset:offset+limit]
         with_user_ids = [a.with_user_id for a in activities_db if a.with_user_id]
--- media/js/newsblur/social_page/social_page_story.js
+++ media/js/newsblur/social_page/social_page_story.js
@@ -11,7 +11,9 @@ NEWSBLUR.Views.SocialPageStory = Backbone.View.extend({
         "focus .NB-story-comment-input"     : "focus_comment_input",
         // "blur .NB-story-comment-input"      : "blur_comment_input"
         "keyup .NB-story-comment-input"     : "keypress_comment_input",
-        "click .NB-story-comment-save"      : "mark_story_as_shared"
+        "click .NB-story-comment-save"      : "mark_story_as_shared",
+        "click .NB-story-comment-crosspost-twitter"  : "toggle_twitter",
+        "click .NB-story-comment-crosspost-facebook" : "toggle_facebook"
     },
     
     initialize: function() {
@@ -226,6 +228,25 @@ NEWSBLUR.Views.SocialPageStory = Backbone.View.extend({
         }, this));
     },
     
+    check_crosspost_buttons: function() {
+        var $twitter = this.$('.NB-story-comment-crosspost-twitter');
+        var $facebook = this.$('.NB-story-comment-crosspost-facebook');
+
+        if (!NEWSBLUR.user_social_services) return;
+        
+        if (NEWSBLUR.user_social_services.twitter &&
+            NEWSBLUR.user_social_services.twitter.twitter_uid) {
+            $twitter.removeClass('NB-hidden');
+        }
+        if (NEWSBLUR.user_social_services.facebook &&
+            NEWSBLUR.user_social_services.facebook.facebook_uid) {
+            $facebook.removeClass('NB-hidden');
+        }
+        
+        $twitter.toggleClass('NB-active', !!NEWSBLUR.assets.preference('post_to_twitter'));
+        $facebook.toggleClass('NB-active', !!NEWSBLUR.assets.preference('post_to_facebook'));
+    },
+    
     // ==========
     // = Events =
     // ==========
@@ -265,7 +286,9 @@ NEWSBLUR.Views.SocialPageStory = Backbone.View.extend({
         // $form.toggleClass('NB-active', $input.is(':focus'));
         $buttons.css('display', 'block');
         $form.addClass('NB-active');
+        this.check_crosspost_buttons();
         this.keypress_comment_input();
+        this.reset_posting_label();
     },
     
     blur_comment_input: function() {
@@ -295,6 +318,66 @@ NEWSBLUR.Views.SocialPageStory = Backbone.View.extend({
         
         var input_width = $input.innerWidth();
         // Perform auto-height expansion
+    },
+    
+    toggle_twitter: function() {
+        var $twitter_button = this.$('.NB-story-comment-crosspost-twitter');
+        
+        if (NEWSBLUR.assets.preference('post_to_twitter')) {
+            NEWSBLUR.assets.preference('post_to_twitter', false);
+        } else {
+            NEWSBLUR.assets.preference('post_to_twitter', true);
+        }
+        
+        $twitter_button.toggleClass('NB-active', NEWSBLUR.assets.preference('post_to_twitter'));
+        this.reset_posting_label();
+    },
+    
+    toggle_facebook: function() {
+        var $facebook_button = this.$('.NB-story-comment-crosspost-facebook');
+        
+        if (NEWSBLUR.assets.preference('post_to_facebook')) {
+            NEWSBLUR.assets.preference('post_to_facebook', false);
+        } else {
+            NEWSBLUR.assets.preference('post_to_facebook', true);
+        }
+        
+        $facebook_button.toggleClass('NB-active', NEWSBLUR.assets.preference('post_to_facebook'));
+        this.reset_posting_label();
+    },
+    
+    show_twitter_posting_label: function() {
+        this.show_posting_label(true, false);
+    },
+    
+    show_facebook_posting_label: function() {
+        this.show_posting_label(false, true);
+    },
+    
+    reset_posting_label: function() {
+        this.show_posting_label();
+    },
+    
+    show_posting_label: function(twitter, facebook) {
+        var social_services = NEWSBLUR.user_social_services || {};
+        var $text = this.$('.NB-story-comment-crosspost-text');
+        twitter = twitter || (social_services.twitter && social_services.twitter.twitter_uid && NEWSBLUR.assets.preference('post_to_twitter'));
+        facebook = facebook || (social_services.facebook && social_services.facebook.facebook_uid && NEWSBLUR.assets.preference('post_to_facebook'));
+        
+        if (twitter || facebook) {
+            var message = "Post to ";
+            if (twitter && !facebook) {
+                message += "Twitter";
+            } else if (!twitter && facebook) {
+                message += "Facebook";
+            } else {
+                message += "Twitter & FB";
+            }
+            
+            $text.text(message);
+        } else {
+            $text.text("");
+        }
     }
     
 });
