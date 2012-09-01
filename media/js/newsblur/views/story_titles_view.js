95326cc83546fc418423ed319f6f8801e77553e1
Samuel Clay <samuel@ofbrooklyn.com>
2012-09-01 14:05:24 -0700

    Scrolling to selected story when re-opening hidden story titles pane. Thanks @afita.

--- media/js/newsblur/views/story_titles_view.js
+++ media/js/newsblur/views/story_titles_view.js
@@ -176,7 +176,7 @@ NEWSBLUR.Views.StoryTitlesView = Backbone.View.extend({
     // ============
     
     scroll_to_selected_story: function(story) {
-        var story_title_view = story.story_title_view || this.collection.active_story.story_title_view;
+        var story_title_view = story && story.story_title_view || this.collection.active_story.story_title_view;
         var story_title_visisble = NEWSBLUR.reader.$s.$story_titles.isScrollVisible(story_title_view.$el);
         if (!story_title_visisble) {
             var container_offset = NEWSBLUR.reader.$s.$story_titles.position().top;
