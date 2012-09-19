0d990ac89a977902d97f8556c342ee0178b590c5
Samuel Clay <samuel@ofbrooklyn.com>
2012-09-19 12:15:05 -0700

    Allowing proxy of S3 for original pages to be turned off.

--- settings.py
+++ settings.py
@@ -420,6 +420,8 @@ BACKED_BY_AWS = {
     'stories_on_dynamodb': False,
 }
 
+PROXY_S3_PAGES = True
+
 # ==================
 # = Configurations =
 # ==================
@@ -438,7 +440,6 @@ AWS_SECRET_ACCESS_KEY = S3_SECRET
 S3_BACKUP_BUCKET = 'newsblur_backups'
 S3_PAGES_BUCKET_NAME = 'pages.newsblur.com'
 S3_ICONS_BUCKET_NAME = 'icons.newsblur.com'
-PROXY_S3_PAGES = True
 
 def custom_show_toolbar(request):
     return DEBUG
