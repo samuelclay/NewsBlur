de18a69ecf86b24ad574502a56f5ede5828b66fa
Samuel Clay <samuel@ofbrooklyn.com>
2013-01-09 15:49:33 -0800

    Fixing two small bugs in text view.

--- media/js/newsblur/reader/reader.js
+++ media/js/newsblur/reader/reader.js
@@ -749,17 +749,21 @@
                 dir = '-';
             }
             // NEWSBLUR.log(['page_in_story', this.$s.$story_pane, direction, page_height, scroll_height]);
-            if (this.story_view == 'page' && !this.flags['page_view_showing_feed_view']) {
+            if (this.story_view == 'page' && 
+                !this.flags['page_view_showing_feed_view'] &&
+                !this.flags['temporary_story_view']) {
                 this.$s.$feed_iframe.scrollTo({
                     top: dir+'='+scroll_height, 
                     left:'+=0'
                 }, 230, {queue: false});
-            } else if (this.story_view == 'feed' || this.flags['page_view_showing_feed_view']) {
+            } else if (this.story_view == 'feed' || 
+                       this.flags['page_view_showing_feed_view']) {
                 this.$s.$feed_stories.scrollTo({
                     top: dir+'='+scroll_height, 
                     left:'+=0'
                 }, 230, {queue: false});
-            } else if (this.story_view == 'text') {
+            } else if (this.story_view == 'text' ||
+                       this.flags['temporary_story_view']) {
                 this.$s.$text_view.scrollTo({
                     top: dir+'='+scroll_height, 
                     left:'+=0'
@@ -4019,7 +4023,7 @@
                 this.story_view == 'text' ||
                 this.flags['feed_view_showing_story_view'] ||
                 this.flags['temporary_story_view']) {
-                // this.hide_mouse_indicator();
+                this.hide_mouse_indicator();
             } else {
                 _.delay(_.bind(this.show_mouse_indicator, this), 350);
             }
