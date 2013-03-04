04f4284ef9284cd6278c42acc147eccbf02b023c
Samuel Clay <samuel@ofbrooklyn.com>
2013-03-04 11:15:32 -0800

    ?next=account

--- media/js/newsblur/views/feed_list_view.js
+++ media/js/newsblur/views/feed_list_view.js
@@ -170,6 +170,8 @@ NEWSBLUR.Views.FeedList = Backbone.View.extend({
             NEWSBLUR.reader.open_goodies_modal();
         } else if (next == 'friends') {
             NEWSBLUR.reader.open_friends_modal();
+        } else if (next == 'account') {
+            NEWSBLUR.reader.open_account_modal();
         } else if (next == 'chooser') {
             NEWSBLUR.reader.open_feedchooser_modal();
         } else if (next == 'password') {
