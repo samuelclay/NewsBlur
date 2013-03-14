0446de3df11c57d34342e483774978de87d828fb
Samuel Clay <samuel@ofbrooklyn.com>
2013-03-13 21:55:57 -0700

    Moving original page server to redis server.

--- settings.py
+++ settings.py
@@ -442,7 +442,7 @@ TWITTER_CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 # = AWS Backing =
 # ===============
 
-ORIGINAL_PAGE_SERVER = "db01.newsblur.com:3060"
+ORIGINAL_PAGE_SERVER = "db02.newsblur.com:3060"
 
 BACKED_BY_AWS = {
     'pages_on_s3': False,
