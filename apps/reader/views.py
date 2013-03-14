38bd6be2464636a7ad9c3e8a2a7da952d789ffcd
Samuel Clay <samuel@ofbrooklyn.com>
2013-03-14 09:59:42 -0700

    Paywall on signup.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -159,7 +159,9 @@ def signup(request):
             new_user = form.save()
             login_user(request, new_user)
             logging.user(new_user, "~FG~SB~BBNEW SIGNUP~FW")
-            return HttpResponseRedirect(reverse('index'))
+            url = "https://%s%s" % (Site.objects.get_current().domain,
+                                     reverse('stripe-form'))
+            return HttpResponseRedirect(url)
 
     return index(request)
         
