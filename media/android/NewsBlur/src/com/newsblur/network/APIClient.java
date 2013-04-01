b23370c52dcd1fbdc677f97890a922c72c7df259
ojiikun <ojiikun@gmail.com>
2013-04-01 23:47:40 +0000

    Undo whitespace changes in APIClient.

--- media/android/NewsBlur/src/com/newsblur/network/APIClient.java
+++ media/android/NewsBlur/src/com/newsblur/network/APIClient.java
@@ -23,13 +23,12 @@ import com.newsblur.util.PrefConstants;
 
 public class APIClient {
 
-    private Context context;
+	private Context context;
 
-    public APIClient(final Context context) {
-        this.context = context;
-    }
+	public APIClient(final Context context) {
+		this.context = context;
+	}
 
-<<<<<<< HEAD
 	public APIResponse get(final String urlString) {
 		HttpURLConnection connection = null;
 		if (!NetworkUtils.isOnline(context)) {
@@ -119,115 +118,23 @@ public class APIClient {
 			connection.disconnect();
 		}
 	}
-=======
-    public APIResponse get(final String urlString) {
-        HttpURLConnection connection = null;
-        if (!NetworkUtils.isOnline(context)) {
-            APIResponse response = new APIResponse();
-            response.isOffline = true;
-            return response;
-        }
-        try {
-            final URL url = new URL(urlString);
-            Log.d(this.getClass().getName(), "API GET: " + url);
-            connection = (HttpURLConnection) url.openConnection();
-            final SharedPreferences preferences = context.getSharedPreferences(PrefConstants.PREFERENCES, 0);
-            final String cookie = preferences.getString(PrefConstants.PREF_COOKIE, null);
-            if (cookie != null) {
-                connection.setRequestProperty("Cookie", cookie);
-            }
-            return extractResponse(url, connection);
-        } catch (IOException e) {
-            Log.e(this.getClass().getName(), "Error opening GET connection to " + urlString, e.getCause());
-            return new APIResponse();
-        } finally {
-            connection.disconnect();
-        }
-    }
-    
-    public APIResponse get(final String urlString, final ContentValues values) {
-        HttpURLConnection connection = null;
-        if (!NetworkUtils.isOnline(context)) {
-            APIResponse response = new APIResponse();
-            response.isOffline = true;
-            return response;
-        }
-        try {
-            List<String> parameters = new ArrayList<String>();
-            for (Entry<String, Object> entry : values.valueSet()) {
-                final StringBuilder builder = new StringBuilder();
-                builder.append((String) entry.getKey());
-                builder.append("=");
-                builder.append(URLEncoder.encode((String) entry.getValue()));
-                parameters.add(builder.toString());
-            }
-            final String parameterString = TextUtils.join("&", parameters);
 
-            final URL url = new URL(urlString + "?" + parameterString);
-            Log.d(this.getClass().getName(), "API GET: " + url);
-            connection = (HttpURLConnection) url.openConnection();
-            final SharedPreferences preferences = context.getSharedPreferences(PrefConstants.PREFERENCES, 0);
-            final String cookie = preferences.getString(PrefConstants.PREF_COOKIE, null);
-            if (cookie != null) {
-                connection.setRequestProperty("Cookie", cookie);
-            }
-            return extractResponse(url, connection);
-        } catch (IOException e) {
-            Log.e(this.getClass().getName(), "Error opening GET connection to " + urlString, e.getCause());
-            return new APIResponse();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
-    
-    public APIResponse get(final String urlString, final ValueMultimap valueMap) {
-        HttpURLConnection connection = null;
-        if (!NetworkUtils.isOnline(context)) {
-            APIResponse response = new APIResponse();
-            response.isOffline = true;
-            return response;
-        }
-        try {
-            String parameterString = valueMap.getParameterString();
+	private APIResponse extractResponse(final URL url, HttpURLConnection connection) throws IOException {
+		StringBuilder builder = new StringBuilder();
+		final Scanner scanner = new Scanner(connection.getInputStream());
+		while (scanner.hasNextLine()) {
+			builder.append(scanner.nextLine());
+		}
+		
+		final APIResponse response = new APIResponse();
+		response.responseString = builder.toString();
+		response.responseCode = connection.getResponseCode();
+		response.cookie = connection.getHeaderField("Set-Cookie");
+		response.hasRedirected = !TextUtils.equals(url.getHost(), connection.getURL().getHost());
 
-            final URL url = new URL(urlString + "?" + parameterString);
-            Log.d(this.getClass().getName(), "API GET: " + url);
-            connection = (HttpURLConnection) url.openConnection();
+		return response;
+	}
 
-            final SharedPreferences preferences = context.getSharedPreferences(PrefConstants.PREFERENCES, 0);
-            final String cookie = preferences.getString(PrefConstants.PREF_COOKIE, null);
-            if (cookie != null) {
-                connection.setRequestProperty("Cookie", cookie);
-            }
-            return extractResponse(url, connection);
-        } catch (IOException e) {
-            Log.e(this.getClass().getName(), "Error opening GET connection to " + urlString, e.getCause());
-            return new APIResponse();
-        } finally {
-            connection.disconnect();
-        }
-    }
->>>>>>> Remove extra debug.  Add full API logging.
-
-    private APIResponse extractResponse(final URL url, HttpURLConnection connection) throws IOException {
-        StringBuilder builder = new StringBuilder();
-        final Scanner scanner = new Scanner(connection.getInputStream());
-        while (scanner.hasNextLine()) {
-            builder.append(scanner.nextLine());
-        }
-        
-        final APIResponse response = new APIResponse();
-        response.responseString = builder.toString();
-        response.responseCode = connection.getResponseCode();
-        response.cookie = connection.getHeaderField("Set-Cookie");
-        response.hasRedirected = !TextUtils.equals(url.getHost(), connection.getURL().getHost());
-
-        return response;
-    }
-
-<<<<<<< HEAD
 	public APIResponse post(final String urlString, final ContentValues values) {
 		HttpURLConnection connection = null;
 		if (!NetworkUtils.isOnline(context)) {
@@ -311,100 +218,15 @@ public class APIClient {
 			final PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
 			printWriter.print(parameterString);
 			printWriter.close();
-=======
-    public APIResponse post(final String urlString, final ContentValues values) {
-        HttpURLConnection connection = null;
-        if (!NetworkUtils.isOnline(context)) {
-            APIResponse response = new APIResponse();
-            response.isOffline = true;
-            return response;
-        }
-        List<String> parameters = new ArrayList<String>();
-        for (Entry<String, Object> entry : values.valueSet()) {
-            final StringBuilder builder = new StringBuilder();
-            
-            builder.append((String) entry.getKey());
-            builder.append("=");
-            try {
-                builder.append(URLEncoder.encode((String) entry.getValue(), "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                Log.e(this.getClass().getName(), e.getLocalizedMessage());
-                return new APIResponse();
-            }
-            parameters.add(builder.toString());
-        }
-        final String parameterString = TextUtils.join("&", parameters);
-        try {
-            final URL url = new URL(urlString);
-            Log.d(this.getClass().getName(), "API POST: " + url);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setFixedLengthStreamingMode(parameterString.getBytes().length);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            
-            final SharedPreferences preferences = context.getSharedPreferences(PrefConstants.PREFERENCES, 0);
-            final String cookie = preferences.getString(PrefConstants.PREF_COOKIE, null);
-            if (cookie != null) {
-                connection.setRequestProperty("Cookie", cookie);
-            }
-            
-            final PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
-            printWriter.print(parameterString);
-            printWriter.close();
 
-            return extractResponse(url, connection);
-        } catch (IOException e) {
-            Log.e(this.getClass().getName(), "Error opening POST connection to " + urlString + ": " + e.getCause(), e.getCause());
-            return new APIResponse();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
-    
-    public APIResponse post(final String urlString, final ValueMultimap valueMap) {
-        return post(urlString, valueMap, true);
-    }
-    
-    public APIResponse post(final String urlString, final ValueMultimap valueMap, boolean jsonIfy) {
-        HttpURLConnection connection = null;
-        if (!NetworkUtils.isOnline(context)) {
-            APIResponse response = new APIResponse();
-            response.isOffline = true;
-            return response;
-        }
-        
-        try {
-            final URL url = new URL(urlString);
-            Log.d(this.getClass().getName(), "API POST: " + url);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            String parameterString = jsonIfy ? valueMap.getJsonString() : valueMap.getParameterString();
-            connection.setFixedLengthStreamingMode(parameterString.getBytes().length);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            
-            final SharedPreferences preferences = context.getSharedPreferences(PrefConstants.PREFERENCES, 0);
-            final String cookie = preferences.getString(PrefConstants.PREF_COOKIE, null);
-            if (cookie != null) {
-                connection.setRequestProperty("Cookie", cookie);
-            }
-            
-            final PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
-            printWriter.print(parameterString);
-            printWriter.close();
->>>>>>> Remove extra debug.  Add full API logging.
-
-            return extractResponse(url, connection);
-        } catch (IOException e) {
-            Log.e(this.getClass().getName(), "Error opening POST connection to " + urlString + ": " + e.getCause(), e.getCause());
-            return new APIResponse();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
+			return extractResponse(url, connection);
+		} catch (IOException e) {
+			Log.e(this.getClass().getName(), "Error opening POST connection to " + urlString + ": " + e.getCause(), e.getCause());
+			return new APIResponse();
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+	}
 }
