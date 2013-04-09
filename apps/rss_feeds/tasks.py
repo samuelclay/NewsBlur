a6837724e34a5cdab8aa39a36a8f35c74ad40f08
Samuel Clay <samuel@ofbrooklyn.com>
2013-04-08 17:11:40 -0700

    Increasing task queue size.

--- apps/rss_feeds/tasks.py
+++ apps/rss_feeds/tasks.py
@@ -33,8 +33,8 @@ class TaskFeeds(Task):
                         r.zcard('scheduled_updates')))
         
         # Regular feeds
-        if tasked_feeds_size < 1000:
-            feeds = r.srandmember('queued_feeds', 1000)
+        if tasked_feeds_size < 2000:
+            feeds = r.srandmember('queued_feeds', 1500)
             Feed.task_feeds(feeds, verbose=True)
             active_count = len(feeds)
         else:
