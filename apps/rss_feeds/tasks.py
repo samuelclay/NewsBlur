6a3d043cede3eeb1cad534209bb052accf537979
Samuel Clay <samuel@ofbrooklyn.com>
2013-04-23 21:17:03 -0700

    Upping to 5000 feeds per minute.

--- apps/rss_feeds/tasks.py
+++ apps/rss_feeds/tasks.py
@@ -33,8 +33,8 @@ class TaskFeeds(Task):
                         r.zcard('scheduled_updates')))
         
         # Regular feeds
-        if tasked_feeds_size < 2000:
-            feeds = r.srandmember('queued_feeds', 2000)
+        if tasked_feeds_size < 5000:
+            feeds = r.srandmember('queued_feeds', 5000)
             Feed.task_feeds(feeds, verbose=True)
             active_count = len(feeds)
         else:
