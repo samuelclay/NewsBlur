38e9d4cbd4b9e9e58273213efb80afb0283fb27f
Thomas Weißschuh <thomas@t-8ch.de>
2013-04-26 07:21:25 +0000

    android: remove AppConstants.NEWSBLUR_URL

--- media/android/NewsBlur/src/com/newsblur/util/AppConstants.java
+++ media/android/NewsBlur/src/com/newsblur/util/AppConstants.java
@@ -10,11 +10,10 @@ public class AppConstants {
 	public static final int REGISTRATION_COMPLETED = 1;
 	
 	public static final String FOLDER_PRE = "folder_collapsed";
-	public static final String NEWSBLUR_URL = "http://www.newsblur.com";
 	public static final float FONT_SIZE_LOWER_BOUND = 1.0f;
 	public static final float FONT_SIZE_INCREMENT_FACTOR = 5;
 	
     // the name to give the "root" folder in the local DB since the API does not assign it one.
     // this name should be unique and such that it will sort to the beginning of a list, ideally.
-    public static final String ROOT_FOLDER = "0000_TOP_LEVEL_";
+	public static final String ROOT_FOLDER = "0000_TOP_LEVEL_";
 }
