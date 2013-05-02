009010f879cbd4e1f652405ef9149d9fd8ca3e79
Samuel Clay <samuel@ofbrooklyn.com>
2013-05-02 12:27:37 -0700

    Removing unused redis story objectid db.

--- settings.py
+++ settings.py
@@ -545,7 +545,6 @@ MONGOANALYTICSDB = connect(MONGO_ANALYTICS_DB.pop('name'), **MONGO_ANALYTICS_DB)
 # =========
 
 REDIS_POOL = redis.ConnectionPool(host=REDIS['host'], port=6379, db=0)
-REDIS_STORY_POOL = redis.ConnectionPool(host=REDIS['host'], port=6379, db=1)
 REDIS_ANALYTICS_POOL = redis.ConnectionPool(host=REDIS['host'], port=6379, db=2)
 REDIS_STATISTICS_POOL = redis.ConnectionPool(host=REDIS['host'], port=6379, db=3)
 REDIS_FEED_POOL = redis.ConnectionPool(host=REDIS['host'], port=6379, db=4)
