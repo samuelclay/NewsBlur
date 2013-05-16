950a15938740a849bb44d9a7fb98f7940389d8f0
Samuel Clay <samuel@ofbrooklyn.com>
2013-05-15 17:57:09 -0700

    Removing PyYAML as a non-vendorized dependency.

--- fabfile.py
+++ fabfile.py
@@ -370,7 +370,7 @@ def setup_task(queue=None, skip_common=False):
 def setup_installs():
     sudo('apt-get -y update')
     sudo('apt-get -y upgrade')
-    sudo('apt-get -y install build-essential gcc scons libreadline-dev sysstat iotop git python-dev locate python-software-properties software-properties-common libpcre3-dev libncurses5-dev libdbd-pg-perl libssl-dev make pgbouncer python-psycopg2 libyaml-0-2 python-numpy python-scipy python-imaging curl monit ufw')
+    sudo('apt-get -y install build-essential gcc scons libreadline-dev sysstat iotop git python-dev locate python-software-properties software-properties-common libpcre3-dev libncurses5-dev libdbd-pg-perl libssl-dev make pgbouncer python-psycopg2 libyaml-0-2 python-yaml python-numpy python-scipy python-imaging curl monit ufw')
 
     # sudo('add-apt-repository ppa:pitti/postgresql')
     sudo('apt-get -y update')
