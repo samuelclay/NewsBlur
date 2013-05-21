fbb908174abdac3a2f77a5008cffb4f037306375
Samuel Clay <samuel@ofbrooklyn.com>
2013-05-21 06:34:49 -0700

    Stripping www from blurblogs.

--- utils/templatetags/utils_tags.py
+++ utils/templatetags/utils_tags.py
@@ -11,11 +11,13 @@ from utils.user_functions import get_user
 register = template.Library()
 
 @register.simple_tag
-def current_domain(dev=False):
+def current_domain(dev=False, strip_www=False):
     current_site = Site.objects.get_current()
     domain = current_site and current_site.domain
     if dev and settings.SERVER_NAME in ["dev"] and domain:
         domain = domain.replace("www", "dev")
+    if strip_www:
+        domain = domain.replace("www.", "")
     return domain
 
 @register.simple_tag(takes_context=True)
