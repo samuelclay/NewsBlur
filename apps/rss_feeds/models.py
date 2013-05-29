5dc1342e1c6a361f8baac3c434517a6af4ca16ff
Samuel Clay <samuel@ofbrooklyn.com>
2013-05-29 16:05:56 -0700

    If a feed has an error, save up to 50 fetches before declaring inactive. Otherwise, only keep 5 good fetches. Thanks to @bdarnell for the suggestion.

--- apps/rss_feeds/models.py
+++ apps/rss_feeds/models.py
@@ -1825,7 +1825,10 @@ class MFetchHistory(mongo.Document):
             history = fetch_history.push_history or []
 
         history.insert(0, (date, code, message))
-        history = history[:5]
+        if code and code >= 400:
+            history = history[:50]
+        else:
+            history = history[:5]
 
         if fetch_type == 'feed':
             fetch_history.feed_fetch_history = history
