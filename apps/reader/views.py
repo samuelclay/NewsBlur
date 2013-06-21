a38d73a3f3b18326e9050004f56b7cd26b5a4bd1
Samuel Clay <samuel@ofbrooklyn.com>
2013-06-21 12:49:30 -0700

    Better error handling on mark_feed_as_read.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -1098,6 +1098,7 @@ def mark_feed_as_read(request):
     feed_ids = request.REQUEST.getlist('feed_id')
     multiple = len(feed_ids) > 1
     code = 1
+    errors = []
     
     for feed_id in feed_ids:
         if 'social:' in feed_id:
@@ -1112,23 +1113,29 @@ def mark_feed_as_read(request):
                 sub = UserSubscription.objects.get(feed=feed, user=request.user)
                 if not multiple:
                     logging.user(request, "~FMMarking feed as read: ~SB%s" % (feed,))
-            except (Feed.DoesNotExist, UserSubscription.DoesNotExist):
+            except (Feed.DoesNotExist, UserSubscription.DoesNotExist), e:
+                errors.append("User not subscribed: %s" % e)
+                continue
+            except (ValueError), e:
+                errors.append("Invalid feed_id: %s" % e)
                 continue
 
         if not sub:
+            errors.append("User not subscribed: %s" % feed_id)
             continue
         
         try:
             marked_read = sub.mark_feed_read()
             if marked_read:
                 r.publish(request.user.username, 'feed:%s' % feed_id)
-        except IntegrityError:
+        except IntegrityError, e:
+            errors.append("Could not mark feed as read: %s" % e)
             code = -1
             
     if multiple:
         logging.user(request, "~FMMarking ~SB%s~SN feeds as read" % len(feed_ids))
         
-    return dict(code=code)
+    return dict(code=code, errors=errors)
 
 def _parse_user_info(user):
     return {
