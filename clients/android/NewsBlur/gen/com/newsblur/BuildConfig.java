d941c994f8f81e532a3dd7a37321932fefaf44d5
Samuel Clay <samuel@ofbrooklyn.com>
2013-07-03 13:05:42 -0700

    Updating android to work with android studio. Version 2.1.

---
+++ clients/android/NewsBlur/gen/com/newsblur/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.newsblur;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
