893f5511b917deec96306e1944709401a1ff9493
Samuel Clay <samuel@ofbrooklyn.com>
2013-07-09 20:06:22 -0700

    Ensuring only non-branched feeds show up in autocomplete.

--- apps/rss_feeds/views.py
+++ apps/rss_feeds/views.py
@@ -94,6 +94,7 @@ def feed_autocomplete(request):
         
     feed_ids = Feed.autocomplete(query)
     feeds = [Feed.get_by_id(feed_id) for feed_id in feed_ids]
+    feeds = [feed for feed in feeds if not feed.branch_from_feed]
     if format == 'autocomplete':
         feeds = [{
             'id': feed.pk,
