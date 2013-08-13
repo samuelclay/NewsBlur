b653653b7f6e3dabad8b9c154a162ba2339ddc62
Samuel Clay <samuel@ofbrooklyn.com>
2013-08-12 17:15:58 -0700

    Handing out free accounts a little quicker.

--- settings.py
+++ settings.py
@@ -381,7 +381,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'activate-next-new-user': {
         'task': 'activate-next-new-user',
-        'schedule': datetime.timedelta(minutes=5),
+        'schedule': datetime.timedelta(minutes=3.5),
         'options': {'queue': 'beat_tasks'},
     },
 }
