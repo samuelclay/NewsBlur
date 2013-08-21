ce4bd81d9d8770f392b9b471cd1def1d4e4b7f84
Samuel Clay <samuel@ofbrooklyn.com>
2013-08-21 13:37:54 -0700

    Upping delay on new user queue.

--- settings.py
+++ settings.py
@@ -384,7 +384,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'activate-next-new-user': {
         'task': 'activate-next-new-user',
-        'schedule': datetime.timedelta(minutes=3.5),
+        'schedule': datetime.timedelta(minutes=10),
         'options': {'queue': 'beat_tasks'},
     },
 }
