c448d8731b152f02bb26f784bc35a3711a7ef0a4
Samuel Clay <samuel@ofbrooklyn.com>
2013-08-23 17:29:32 -0700

    Fixing unknown host error on DO servers.

--- fabfile.py
+++ fabfile.py
@@ -14,6 +14,7 @@ import os
 import time
 import sys
 import re
+
 try:
     import dop.client
 except ImportError:
@@ -439,6 +440,7 @@ def setup_supervisor():
 @parallel
 def setup_hosts():
     put(os.path.join(env.SECRETS_PATH, 'configs/hosts'), '/etc/hosts', use_sudo=True)
+    sudo('echo "\n\n127.0.0.1   `hostname`" >> /etc/hosts')
 
 def config_pgbouncer():
     put('config/pgbouncer.conf', '/etc/pgbouncer/pgbouncer.ini', use_sudo=True)
