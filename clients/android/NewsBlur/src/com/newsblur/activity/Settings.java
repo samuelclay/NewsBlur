94d41de71ce9bd71bf91cd71850c2a8cbb662742
Mark Anderson <manderson23@gmail.com>
2013-10-18 21:00:13 +0100

    Support navigating back to home page from action bar in Preferences

--- clients/android/NewsBlur/src/com/newsblur/activity/Settings.java
+++ clients/android/NewsBlur/src/com/newsblur/activity/Settings.java
@@ -1,6 +1,7 @@
 package com.newsblur.activity;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.newsblur.R;
 import com.newsblur.util.PrefConstants;
 
@@ -11,8 +12,19 @@ public class Settings extends SherlockPreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         super.getPreferenceManager().setSharedPreferencesName(PrefConstants.PREFERENCES);
         addPreferencesFromResource(R.layout.activity_settings);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);   
+        }
+    }
 }
