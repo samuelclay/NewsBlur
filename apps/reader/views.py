b5d1d74bac2f08809e269ee817ac96f564a03633
Samuel Clay <samuel@ofbrooklyn.com>
2013-10-31 09:59:05 -0700

    Turning off upgrade message in iOS.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -307,7 +307,7 @@ def load_feeds_flat(request):
     
     feeds = {}
     flat_folders = {" ": []}
-    iphone_version = "3.0"
+    iphone_version = "2.1"
     
     if include_favicons == 'false': include_favicons = False
     if update_counts == 'false': update_counts = False
