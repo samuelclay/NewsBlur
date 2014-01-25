e468919c276dc61fef53163c8467e1a84a1bec4e
Samuel Clay <samuel@ofbrooklyn.com>
2014-01-24 16:52:12 -0800

    Adding ifttt specific urls.

--- apps/oauth/urls.py
+++ apps/oauth/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, patterns
 from apps.oauth import views
+from oauth2_provider import views as op_views
 
 urlpatterns = patterns('',
     url(r'^twitter_connect/?$', views.twitter_connect, name='twitter-connect'),
@@ -10,4 +11,8 @@ urlpatterns = patterns('',
     url(r'^appdotnet_disconnect/?$', views.appdotnet_disconnect, name='appdotnet-disconnect'),
     url(r'^follow_twitter_account/?$', views.follow_twitter_account, name='social-follow-twitter'),
     url(r'^unfollow_twitter_account/?$', views.unfollow_twitter_account, name='social-unfollow-twitter'),
+
+    # Django OAuth Toolkit
+    url(r'^authorize/?$', op_views.AuthorizationView.as_view(), name="ifttt-authorize"),
+    url(r'^token/?$', op_views.TokenView.as_view(), name="ifttt-token"),
 )
