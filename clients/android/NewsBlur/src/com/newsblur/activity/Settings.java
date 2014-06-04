7c1f7ad647fedfe3921fe2324ea597778bcf3ede
Mark Anderson <manderson23@gmail.com>
2014-06-04 22:00:31 +0100

    Switch theme automatically following preference change

--- clients/android/NewsBlur/src/com/newsblur/activity/Settings.java
+++ clients/android/NewsBlur/src/com/newsblur/activity/Settings.java
@@ -3,14 +3,17 @@ package com.newsblur.activity;
 import com.newsblur.R;
 import com.newsblur.util.PrefConstants;
 import com.newsblur.util.PrefsUtils;
+import com.newsblur.util.UIUtils;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
-public class Settings extends PreferenceActivity {
+public class Settings extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,10 @@ public class Settings extends PreferenceActivity {
 
         super.onCreate(savedInstanceState);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        super.getPreferenceManager().setSharedPreferencesName(PrefConstants.PREFERENCES);
+        PreferenceManager preferenceManager = super.getPreferenceManager();
+        preferenceManager.setSharedPreferencesName(PrefConstants.PREFERENCES);
+        SharedPreferences prefs = getSharedPreferences(PrefConstants.PREFERENCES, 0);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         addPreferencesFromResource(R.layout.activity_settings);
 
         // Remove the reading category of references on pre-4.4 devices as it only contains
@@ -30,6 +36,14 @@ public class Settings extends PreferenceActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences prefs = getSharedPreferences(PrefConstants.PREFERENCES, 0);
+        prefs.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case android.R.id.home:
@@ -37,6 +51,13 @@ public class Settings extends PreferenceActivity {
             return true;
         default:
             return super.onOptionsItemSelected(item);   
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(PrefConstants.THEME)) {
+            UIUtils.restartActivity(this);
         }
     }
 }
