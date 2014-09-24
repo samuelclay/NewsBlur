55a03018f7c27610884ed98e325884a112d0a9e8
Samuel Clay <samuel@ofbrooklyn.com>
2014-09-24 12:37:13 -0700

    NewsBlur iOS v4.5 beta 3. With iPhone 6/6+ support.

--- clients/ios/Classes/FirstTimeUserViewController.m
+++ clients/ios/Classes/FirstTimeUserViewController.m
@@ -85,10 +85,12 @@
     
     UIImage *logoImg = [UIImage imageNamed:@"logo_512"];
     UIImageView *logoView = [[UIImageView alloc] initWithImage:logoImg];
+    CGFloat width = CGRectGetWidth(self.view.frame);
+    CGFloat height = CGRectGetHeight(self.view.frame);
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-        logoView.frame = CGRectMake(50, 100, 220, 220);
+        logoView.frame = CGRectMake((width-220)/2, (height-220)/2-50, 220, 220);
     } else {
-        logoView.frame = CGRectMake(150, 99, 240, 240);
+        logoView.frame = CGRectMake((width-240)/2, (height-240)/2-50, 240, 240);
     }
     
     self.logo = logoView;
