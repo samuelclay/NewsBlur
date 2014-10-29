b91b65772aceb8e681e54a27837b72d6bf67c64e
Samuel Clay <samuel@ofbrooklyn.com>
2014-10-29 16:16:50 -0700

    Adding proxy for Story view to handle https loading http sites. Needs testing, but done.

--- media/js/newsblur/reader/reader.js
+++ media/js/newsblur/reader/reader.js
@@ -2766,9 +2766,10 @@
                     'duration': this.model.preference('animations') ? 550 : 0,
                     'queue': false
                 });
-                NEWSBLUR.app.story_tab_view.load_story_iframe();
                 if (!this.active_story) {
                     NEWSBLUR.app.story_tab_view.show_explainer_single_story_mode();                        
+                } else {
+                    NEWSBLUR.app.story_tab_view.open_story();
                 }
             }
             
@@ -5927,7 +5928,8 @@
                 if (self.flags['feed_view_showing_story_view']) {
                     self.switch_to_correct_view();
                 } else {
-                    NEWSBLUR.app.story_tab_view.open_story(null, true);
+                    NEWSBLUR.app.story_tab_view.prepare_story(null, true);
+                    NEWSBLUR.app.story_tab_view.open_story();
                 }
             });
             $document.bind('keydown', 'return', function(e) {
@@ -5935,7 +5937,8 @@
                 if (self.flags['feed_view_showing_story_view']) {
                     self.switch_to_correct_view();
                 } else {
-                    NEWSBLUR.app.story_tab_view.open_story(null, true);
+                    NEWSBLUR.app.story_tab_view.prepare_story(null, true);
+                    NEWSBLUR.app.story_tab_view.open_story();
                 }
             });
             $document.bind('keydown', 'shift+enter', function(e) {
