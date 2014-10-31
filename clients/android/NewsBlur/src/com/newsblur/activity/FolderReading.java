96a9f3d2988bea0201d6e7f9e6ebf754cbf94ee4
dosiecki <ojiikun@gmail.com>
2014-10-31 01:42:35 -0700

    Remove unused import.

--- clients/android/NewsBlur/src/com/newsblur/activity/FolderReading.java
+++ clients/android/NewsBlur/src/com/newsblur/activity/FolderReading.java
@@ -6,7 +6,6 @@ import android.content.CursorLoader;
 import android.content.Loader;
 
 import com.newsblur.database.DatabaseConstants;
-import com.newsblur.database.FeedProvider;
 import com.newsblur.database.MixedFeedsReadingAdapter;
 import com.newsblur.util.FeedUtils;
 import com.newsblur.util.PrefsUtils;
