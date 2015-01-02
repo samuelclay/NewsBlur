samuelclay/NewsBlur

commit 1002403e81087e16695589e7ad1b322856252d40

Fixing activities to only use URL and title, except Mail which uses the whole body.

clients/ios/Classes/NBActivityItemProvider.m
@@ -1,0 +1,46 @@
+//
+//  NBActivityItemProvider.m
+//  NewsBlur
+//
+//  Created by Samuel Clay on 12/15/14.
+//  Copyright (c) 2014 NewsBlur. All rights reserved.
+//
+
+#import "NBActivityItemProvider.h"
+
+@implementation NBActivityItemProvider
+
+- (instancetype)initWithUrl:(NSURL *)_url authorName:(NSString *)_authorName text:(NSString *)_text title:(NSString *)_title feedTitle:(NSString *)_feedTitle images:(NSArray *)_images {
+    if (self = [self init]) {
+        url = _url;
+        authorName = _authorName;
+        text = _text;
+        title = _title;
+        feedTitle = _feedTitle;
+        images = _images;
+        
+    }
+    return self;
+}
+
+- (id)item {
+    return [NSDictionary dictionary];
+}
+
+-(id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(NSString *)activityType {
+    if ([activityType isEqualToString:UIActivityTypeMail] ||
+        [activityType isEqualToString:@"com.evernote.iPhone.Evernote.EvernoteShare"]) {
+        return @{@"body": text, @"subject": title};
+    } else if ([activityType isEqualToString:UIActivityTypePostToTwitter] ||
+               [activityType isEqualToString:UIActivityTypePostToFacebook] ||
+               [activityType isEqualToString:UIActivityTypePostToWeibo]) {
+        return title;
+    }
+    return title;
+}
+
+- (NSString *)activityViewController:(UIActivityViewController *)activityViewController subjectForActivityType:(NSString *)activityType {
+    return title;
+}
+
+@end

clients/ios/Classes/NBActivityItemProvider.h
@@ -1,0 +1,26 @@
+//
+//  NBActivityItemProvider.h
+//  NewsBlur
+//
+//  Created by Samuel Clay on 12/15/14.
+//  Copyright (c) 2014 NewsBlur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBActivityItemProvider : UIActivityItemProvider <UIActivityItemSource> {
+    NSURL *url;
+    NSString *authorName;
+    NSString *text;
+    NSString *title;
+    NSString *feedTitle;
+    NSArray *images;
+}
+
+- (instancetype)initWithUrl:(NSURL *)url
+                 authorName:(NSString *)authorName
+                       text:(NSString *)text
+                      title:(NSString *)title
+                  feedTitle:(NSString *)feedTitle
+                     images:(NSArray *)images;
+@end

clients/ios/Classes/NewsBlurAppDelegate.m
@@ -59,6 +59,7 @@
 #import "UIView+ViewController.h"
 #import "UIViewController+OSKUtilities.h"
 #import "NBURLCache.h"
+#import "NBActivityItemProvider.h"
 #import <float.h>
 
 @implementation NewsBlurAppDelegate
@@ -654,12 +655,18 @@
     } else {
         // iOS 8+
         NSMutableArray *activityItems = [[NSMutableArray alloc] init];
-        if (title) [activityItems addObject:title];
+//        if (title) [activityItems addObject:title];
         if (url) [activityItems addObject:url];
         NSString *maybeFeedTitle = feedTitle ? [NSString stringWithFormat:@" via %@", feedTitle] : @"";
-        if (text) [activityItems addObject:[NSString stringWithFormat:@"<html><body><br><br><hr style=\"border: none; overflow: hidden; height: 1px;width: 100%%;background-color: #C0C0C0;\"><br><a href=\"%@\">%@</a>%@<br>%@</body></html>", [url absoluteString], title, maybeFeedTitle, text]];
+        if (text) text = [NSString stringWithFormat:@"<html><body><br><br><hr style=\"border: none; overflow: hidden; height: 1px;width: 100%%;background-color: #C0C0C0;\"><br><a href=\"%@\">%@</a>%@<br>%@</body></html>", [url absoluteString], title, maybeFeedTitle, text];
     //    if (images) [activityItems addObject:images];
         NSMutableArray *appActivities = [[NSMutableArray alloc] init];
+        [activityItems addObject:[[NBActivityItemProvider alloc] initWithUrl:(NSURL *)url
+                                                              authorName:(NSString *)authorName
+                                                                    text:(NSString *)text
+                                                                   title:(NSString *)title
+                                                               feedTitle:(NSString *)feedTitle
+                                                                  images:(NSArray *)images]];
         if (url) [appActivities addObject:[[TUSafariActivity alloc] init]];
         if (url) [appActivities addObject:[[ARChromeActivity alloc]
                                            initWithCallbackURL:[NSURL URLWithString:@"newsblur://"]]];
@@ -695,12 +702,14 @@
                 _completedString = @"Saved";
             }
             [MBProgressHUD hideHUDForView:vc.view animated:NO];
-            MBProgressHUD *storyHUD = [MBProgressHUD showHUDAddedTo:vc.view animated:YES];
-            storyHUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]];
-            storyHUD.mode = MBProgressHUDModeCustomView;
-            storyHUD.removeFromSuperViewOnHide = YES;
-            storyHUD.labelText = _completedString;
-            [storyHUD hide:YES afterDelay:1];
+            if (completed) {
+                MBProgressHUD *storyHUD = [MBProgressHUD showHUDAddedTo:vc.view animated:YES];
+                storyHUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]];
+                storyHUD.mode = MBProgressHUDModeCustomView;
+                storyHUD.removeFromSuperViewOnHide = YES;
+                storyHUD.labelText = _completedString;
+                [storyHUD hide:YES afterDelay:1];
+            }
         };
         [activityViewController setCompletionHandler:completion];
 

commit 1d22fb9b11c5c560cff2c2fb73ec3a845963643d

Fix crash when mailing a URL in the original story view.
[OriginalStoryViewController doOpenActionSheet] calls showSendTo with a nil
text parameter, but [NBActivityItemProvider activityViewController:
itemForActivityType:] returns a dictionary with the text as a value, and
dictionary literals can't use nil values. Use an empty string instead.

Appears to be a regression from 1002403e81087e16695589e7ad1b322856252d40.

clients/ios/Classes/NBActivityItemProvider.m
@@ -30,7 +30,7 @@
 -(id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(NSString *)activityType {
     if ([activityType isEqualToString:UIActivityTypeMail] ||
         [activityType isEqualToString:@"com.evernote.iPhone.Evernote.EvernoteShare"]) {
-        return @{@"body": text, @"subject": title};
+        return @{@"body": text ?: @"", @"subject": title};
     } else if ([activityType isEqualToString:UIActivityTypePostToTwitter] ||
                [activityType isEqualToString:UIActivityTypePostToFacebook] ||
                [activityType isEqualToString:UIActivityTypePostToWeibo]) {