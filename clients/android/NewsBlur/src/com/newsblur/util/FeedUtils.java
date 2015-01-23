5dd11f84468bf39dfbeb1909d3685dec61197462
dosiecki <ojiikun@gmail.com>
2015-01-22 23:20:32 -0800

    De-async part of mark-all-read to prevent UI from showing before DB is updated. (#612)

--- clients/android/NewsBlur/src/com/newsblur/util/FeedUtils.java
+++ clients/android/NewsBlur/src/com/newsblur/util/FeedUtils.java
@@ -143,6 +143,7 @@ public class FeedUtils {
     }
 
     public static void markFeedsRead(final FeedSet fs, final Long olderThan, final Long newerThan, final Context context) {
+        dbHelper.markStoriesRead(fs, olderThan, newerThan);
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... arg) {
@@ -153,8 +154,6 @@ public class FeedUtils {
                     FeedSet newFeedSet = FeedSet.folder("all", dbHelper.getAllFeeds());
                     ra = ReadingAction.markFeedRead(newFeedSet, olderThan, newerThan);
                 }
-                dbHelper.markStoriesRead(fs, olderThan, newerThan);
-                NbActivity.updateAllActivities();
                 dbHelper.enqueueAction(ra);
                 triggerSync(context);
                 return null;
