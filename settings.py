4d91df308340dcf1d6b2f4b57e9d079ff6b8be7e
Samuel Clay <samuel@ofbrooklyn.com>
2015-03-09 15:50:29 -0700

    Switching sessions db.

--- settings.py
+++ settings.py
@@ -633,7 +633,7 @@ REDIS_POOL                 = redis.ConnectionPool(host=REDIS['host'], port=6379,
 REDIS_ANALYTICS_POOL       = redis.ConnectionPool(host=REDIS['host'], port=6379, db=2)
 REDIS_STATISTICS_POOL      = redis.ConnectionPool(host=REDIS['host'], port=6379, db=3)
 REDIS_FEED_POOL            = redis.ConnectionPool(host=REDIS['host'], port=6379, db=4)
-REDIS_SESSION_POOL         = redis.ConnectionPool(host=REDIS['host'], port=6379, db=5)
+REDIS_SESSION_POOL         = redis.ConnectionPool(host=REDIS_PUBSUB['host'], port=6379, db=5)
 # REDIS_CACHE_POOL         = redis.ConnectionPool(host=REDIS['host'], port=6379, db=6) # Duped in CACHES
 REDIS_PUBSUB_POOL          = redis.ConnectionPool(host=REDIS_PUBSUB['host'], port=6379, db=0)
 REDIS_STORY_HASH_POOL      = redis.ConnectionPool(host=REDIS_STORY['host'], port=6379, db=1)
