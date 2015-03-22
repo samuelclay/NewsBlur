e895d9fbcc3f2603457a0afaa3e660b073b6e0e2
Mark Anderson <manderson23@gmail.com>
2015-03-22 23:12:46 +0000

    Update NetworkUtils to use OkHttpClient

--- clients/android/NewsBlur/src/com/newsblur/util/NetworkUtils.java
+++ clients/android/NewsBlur/src/com/newsblur/util/NetworkUtils.java
@@ -3,16 +3,28 @@ package com.newsblur.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class NetworkUtils {
-	
+
+    private static OkHttpClient httpClient = new OkHttpClient();
+
+    static {
+        // By default OkHttpClient follows redirects (inc HTTP -> HTTPS and HTTPS -> HTTP).
+        httpClient.setConnectTimeout(10, TimeUnit.SECONDS);
+        httpClient.setReadTimeout(30, TimeUnit.SECONDS);
+    }
+
 	public static boolean isOnline(Context context) {
 		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo netInfo = cm.getActiveNetworkInfo();
@@ -20,42 +32,32 @@ public class NetworkUtils {
 	}
 
     public static UrlLoadResult loadURL(URL url, OutputStream outputStream) throws IOException {
-        HttpURLConnection conn = null;
         UrlLoadResult result = new UrlLoadResult();
         try {
-            conn = (HttpURLConnection)url.openConnection();
-            conn.setConnectTimeout(10000);
-            conn.setReadTimeout(30000);
-            conn.setInstanceFollowRedirects(true);
-            int code = conn.getResponseCode();
-            // we explicitly requested redirects, so if we still get one, it is because of a protocol
-            // change. inform the caller by returning the new URL
-            if ((code == HttpURLConnection.HTTP_MOVED_TEMP) || (code == HttpURLConnection.HTTP_MOVED_PERM)) {
-                String loc = conn.getHeaderField("Location");
-                result.redirUrl = loc;
-                return result;
+            Request.Builder requestBuilder = new Request.Builder().url(url);
+            Response response = httpClient.newCall(requestBuilder.build()).execute();
+            if (response.isSuccessful()) {
+                int code = response.code();
+                // we explicitly requested redirects, so if we still get one, it is because of a protocol
+                // change. inform the caller by returning the new URL
+                if ((code == HttpURLConnection.HTTP_MOVED_TEMP) || (code == HttpURLConnection.HTTP_MOVED_PERM)) {
+                    String loc = response.header("Location");
+                    result.redirUrl = loc;
+                    return result;
+                }
+                InputStream inputStream = response.body().byteStream();
+                byte[] b = new byte[1024];
+                int read;
+                while ((read = inputStream.read(b)) != -1) {
+                    outputStream.write(b, 0, read);
+                    result.bytesRead += read;
+                }
             }
-            InputStream inputStream = conn.getInputStream();
-            byte[] b = new byte[1024];
-            int read;
-            while ((read = inputStream.read(b)) != -1) {
-                outputStream.write(b, 0, read);
-                result.bytesRead += read;
-            }
+
         } catch (Throwable t) {
             // a huge number of things could go wrong fetching and storing an image. don't spam logs with them
-        } finally {
-            closeQuietly(conn);
         }
         return result;
-    }
-
-    public static void closeQuietly(HttpURLConnection conn) {
-        if (conn == null) return;
-        try {
-            conn.disconnect();
-        } catch (Throwable t) {
-        }
     }
 
     public static class UrlLoadResult extends Object {
