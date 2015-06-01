76eb50efb09009004c399662989407d9dfb0dc5a
dosiecki <ojiikun@gmail.com>
2015-06-01 16:39:06 -0700

    Don't bleed sync metadata during user switch.

--- clients/android/NewsBlur/src/com/newsblur/util/PrefsUtils.java
+++ clients/android/NewsBlur/src/com/newsblur/util/PrefsUtils.java
@@ -99,6 +99,7 @@ public class PrefsUtils {
 
     public static void logout(Context context) {
         NBSyncService.softInterrupt();
+        NBSyncService.clearState();
 
         // wipe the prefs store
         context.getSharedPreferences(PrefConstants.PREFERENCES, 0).edit().clear().commit();
