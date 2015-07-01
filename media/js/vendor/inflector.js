20e4660336acc01b2324d521c1c09cd57cca0d4f
Samuel Clay <samuel@ofbrooklyn.com>
2015-07-01 16:17:07 -0700

    Fixing striptags to handle broken html that regex can't parse. Thanks to @zacwest for letting me know: https://twitter.com/zacwest/status/616381017130074112

--- media/js/vendor/inflector.js
+++ media/js/vendor/inflector.js
@@ -167,7 +167,7 @@ window.Inflector = {
 
     // From Prototype.js. Strip out HTML tags.
     stripTags : function(s) {
-        return s.replace(/<\w+(\s+("[^"]*"|'[^']*'|[^>])+)?>|<\/\w+>/gi, '');
+        return $('<p>' + s + '</p>').text();
     },
 
     escapeRegExp : function(s) {
