0ea8da6ce6e539df9ada9f7bcbcb2b3411ab498a
Samuel Clay <samuel@ofbrooklyn.com>
2015-08-18 09:00:40 -0700

    Adding log tail to redis bgsave monitor.

--- utils/monitor_redis_bgsave.py
+++ utils/monitor_redis_bgsave.py
@@ -10,12 +10,13 @@ import settings
 import socket
 
 def main():
-    t = os.popen('stat -c%%Y /var/lib/redis/dump.rdb')
+    t = os.popen('stat -c%Y /var/lib/redis/dump.rdb')
     timestamp = t.read().split('\n')[0]
     modified = datetime.datetime.fromtimestamp(int(timestamp))
     ten_min_ago = datetime.datetime.now() - datetime.timedelta(minutes=10)
     hostname = socket.gethostname()
     modified_minutes = datetime.datetime.now() - modified
+    log_tail = os.popen('tail -n 100 /var/log/redis.log').read()
     
     if modified < ten_min_ago:
         requests.post(
@@ -23,10 +24,10 @@ def main():
                 auth=("api", settings.MAILGUN_ACCESS_KEY),
                 data={"from": "NewsBlur Redis Monitor: %s <admin@%s.newsblur.com>" % (hostname, hostname),
                       "to": [settings.ADMINS[0][1]],
-                      "subject": "%s hasn't bgsave'd redis in ten minutes!" % (hostname),
-                      "text": "Last modified %s: %s minutes ago" % (hostname, modified_minutes)})
+                      "subject": "%s hasn't bgsave'd redis in %s!" % (hostname, modified_minutes),
+                      "text": "Last modified %s: %s ago\n\n----\n\n%s" % (hostname, modified_minutes, log_tail)})
     else:
-        print " ---> Redis BGSAve fine: %s / %s ago" % (hostname, modified_minutes)
+        print " ---> Redis bgsave fine: %s / %s ago" % (hostname, modified_minutes)
         
 if __name__ == '__main__':
     main()
