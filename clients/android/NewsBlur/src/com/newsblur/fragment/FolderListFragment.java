a46ee33ab50cf98e55c93d5fb3e87063b4a4bfe3
dosiecki <ojiikun@gmail.com>
2015-09-15 13:52:11 -0700

    Fix context leak from feed list fragment.

--- clients/android/NewsBlur/src/com/newsblur/fragment/FolderListFragment.java
+++ clients/android/NewsBlur/src/com/newsblur/fragment/FolderListFragment.java
@@ -57,7 +57,6 @@ public class FolderListFragment extends NbFragment implements OnCreateContextMen
 	public StateFilter currentState = StateFilter.SOME;
 	private SharedPreferences sharedPreferences;
     @FindView(R.id.folderfeed_list) ExpandableListView list;
-    private Main activity;
     public boolean firstCursorSeenYet = false;
 
 	@Override
@@ -73,7 +72,6 @@ public class FolderListFragment extends NbFragment implements OnCreateContextMen
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 		sharedPreferences = getActivity().getSharedPreferences(PrefConstants.PREFERENCES, 0);
-        activity = (Main) getActivity();
     }
 
 	@Override
@@ -279,7 +277,7 @@ public class FolderListFragment extends NbFragment implements OnCreateContextMen
      * DB model, so having Main also load it would cause some lag.
      */
     public void pushUnreadCounts() {
-        activity.updateUnreadCounts((adapter.totalNeutCount+adapter.totalSocialNeutCount), (adapter.totalPosCount+adapter.totalSocialPosiCount));
+        ((Main) getActivity()).updateUnreadCounts((adapter.totalNeutCount+adapter.totalSocialNeutCount), (adapter.totalPosCount+adapter.totalSocialPosiCount));
     }
 
 	@OnGroupClick(R.id.folderfeed_list) boolean onGroupClick(ExpandableListView list, View group, int groupPosition, long id) {
