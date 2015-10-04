40391feaad7b0cb68c8bfddfa8d133dd15c94a6b
Samuel Clay <samuel@ofbrooklyn.com>
2015-10-03 17:59:35 -0700

    Cleaning up a couple warnings.

--- clients/ios/Classes/NBActivityItemProvider.m
+++ clients/ios/Classes/NBActivityItemProvider.m
@@ -11,7 +11,7 @@
 @implementation NBActivityItemProvider
 
 - (instancetype)initWithUrl:(NSURL *)_url authorName:(NSString *)_authorName text:(NSString *)_text title:(NSString *)_title feedTitle:(NSString *)_feedTitle {
-    if (self = [super initWithPlaceholderItem:_title]) {
+    if (self = [super initWithPlaceholderItem:_url]) {
         url = _url;
         authorName = _authorName;
         text = _text;
