eb4178891328db856a34ab6236f9e5cb648042f1
Samuel Clay <samuel@ofbrooklyn.com>
2016-01-22 12:41:34 -0800

    Boosting oauth authorization code expiration to 1 hour (from 60 seconds) to aid development.

--- settings.py
+++ settings.py
@@ -128,7 +128,8 @@ OAUTH2_PROVIDER = {
         'ifttt': 'Pair your NewsBlur account with other IFTTT channels.',
     },
     'CLIENT_ID_GENERATOR_CLASS': 'oauth2_provider.generators.ClientIdGenerator',
-    'ACCESS_TOKEN_EXPIRE_SECONDS': 60*60*24*365*10 # 10 years
+    'ACCESS_TOKEN_EXPIRE_SECONDS': 60*60*24*365*10, # 10 years
+    'AUTHORIZATION_CODE_EXPIRE_SECONDS': 60*60, # 1 hour
 }
 
 # ===========
