f7bdd5b8c800ac74ee5dfdd9918e00dc1804eecb
Samuel Clay <samuel@ofbrooklyn.com>
2016-02-09 13:47:54 -0800

    Logging sample mailgun requests.

--- apps/newsletters/views.py
+++ apps/newsletters/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, Http404
+from utils import log as logging
+
 
 def newsletter_receive(request):
-    print request.REQUEST
+    logging.debug(request.REQUEST)
     response = HttpResponse('OK')
     return response
