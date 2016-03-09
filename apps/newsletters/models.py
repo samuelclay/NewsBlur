c7dd608885a1a4b7e6a81a72931736956862966e
Samuel Clay <samuel@ofbrooklyn.com>
2016-03-09 11:30:47 -0800

    Fixing missing import for newsletters.

--- apps/newsletters/models.py
+++ apps/newsletters/models.py
@@ -4,6 +4,7 @@ import redis
 from cgi import escape
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.utils.html import linebreaks
