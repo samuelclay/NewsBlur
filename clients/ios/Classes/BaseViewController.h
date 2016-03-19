d1f83c26b277aca5c1a1568a0bc0764b13239e22
Nicholas Riley <com-github@sabi.net>
2016-03-19 13:20:13 -0400

    iOS: Allow Command-. in addition to Escape to go back.
    This doesn’t match the Web but many keyboards for iOS devices, including Apple’s Smart Keyboard, don’t have Escape keys on them.

--- clients/ios/Classes/BaseViewController.h
+++ clients/ios/Classes/BaseViewController.h
@@ -21,6 +21,7 @@
 - (void)informLoadingMessage:(NSString *)message;
 
 - (void)addKeyCommandWithInput:(NSString *)input modifierFlags:(UIKeyModifierFlags)modifierFlags action:(SEL)action discoverabilityTitle:(NSString *)discoverabilityTitle;
+- (void)addCancelKeyCommandWithAction:(SEL)action discoverabilityTitle:(NSString *)discoverabilityTitle;
 
 - (void)updateTheme;
 
