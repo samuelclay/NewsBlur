43ab6bddb114b4ca5304f45b9e4539a4b5d42ba6
Samuel Clay <samuel@ofbrooklyn.com>
2016-03-20 17:57:06 -0700

    Lifetime premium accounts should see status when on Renewal dialog.

--- media/js/newsblur/reader/reader_feedchooser.js
+++ media/js/newsblur/reader/reader_feedchooser.js
@@ -77,7 +77,8 @@ _.extend(NEWSBLUR.ReaderFeedchooser.prototype, {
                       $.make('b', { style: 'display: block; margin: 8px 0' }, [
                           $.make('span', { className: 'NB-raquo' }, '&raquo;'),
                           ' ',
-                          NEWSBLUR.Globals.premium_expire && NEWSBLUR.utils.format_date(NEWSBLUR.Globals.premium_expire)
+                          NEWSBLUR.Globals.premium_expire && NEWSBLUR.utils.format_date(NEWSBLUR.Globals.premium_expire),
+                          (!NEWSBLUR.Globals.premium_expire && $.make('b', "Never gonna expire. Congrats!"))
                       ]),
                       'You can change your payment method and card details. ',
                       (NEWSBLUR.Globals.premium_expire < new Date) ? 
