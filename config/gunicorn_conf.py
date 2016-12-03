a65ec943bf5075ff1529d10263751157eae48174
Samuel Clay <samuel@ofbrooklyn.com>
2016-12-02 19:10:01 -0800

    Updating psutil dependency deprecation.

--- config/gunicorn_conf.py
+++ config/gunicorn_conf.py
@@ -2,7 +2,7 @@ import psutil
 import math
 
 GIGS_OF_MEMORY = psutil.virtual_memory().total/1024/1024/1024.
-NUM_CPUS = psutil.NUM_CPUS
+NUM_CPUS = psutil.cpu_count()
 
 bind = "0.0.0.0:8000"
 pidfile = "/srv/newsblur/logs/gunicorn.pid"
