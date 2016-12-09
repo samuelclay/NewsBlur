4754cc0bfcc4843955fe80ce39dd2b5f4a72496a
Samuel Clay <samuel@ofbrooklyn.com>
2016-12-09 14:54:07 -0800

    Moving site name and image to bottom of folder rss stories. Also adding site title to story title.

--- apps/reader/views.py
+++ apps/reader/views.py
@@ -1091,14 +1091,14 @@ def folder_rss_feed(request, user_id, secret_token, unread_filter, folder_slug):
 
     for story in stories:
         feed = Feed.get_by_id(story['story_feed_id'])
-        story_content = """<img src="//%s/rss_feeds/icon/%s"> %s <br><br> %s""" % (
+        story_content = """%s<br><br><img src="//%s/rss_feeds/icon/%s" width="16" height="16"> %s""" % (
+            smart_unicode(story['story_content'],
             Site.objects.get_current().domain,
             story['story_feed_id'],
-            feed.feed_title if feed else "",
-            smart_unicode(story['story_content'])
+            feed.feed_title if feed else "")
         )
         story_data = {
-            'title': story['story_title'],
+            'title': "%s%s" % (("%s: " % feed.feed_title) if feed else "", story['story_title']),
             'link': story['story_permalink'],
             'description': story_content,
             'categories': story['story_tags'],
