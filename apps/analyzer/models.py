105252228d2d901e34380c85d110d9f5484c808b
Samuel Clay <samuel@ofbrooklyn.com>
2017-01-05 19:25:00 -0800

    Attempting 5000 stories again.

--- apps/analyzer/models.py
+++ apps/analyzer/models.py
@@ -47,7 +47,7 @@ class MPopularityQuery(mongo.Document):
     def queue_email(self):
         EmailPopularityQuery.delay(pk=self.pk)
         
-    def send_email(self, limit=1000):
+    def send_email(self, limit=5000):
         filename = Feed.xls_query_popularity(self.query, limit=limit)
         xlsx = open(filename, "r")
         
