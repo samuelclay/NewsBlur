37c421d77973d97768f119424170a5363cd29691
dosiecki <ojiikun@gmail.com>
2017-05-31 01:18:36 -0700

    remove obsolte compat checks

--- clients/android/NewsBlur/src/com/newsblur/fragment/SettingsFragment.java
+++ clients/android/NewsBlur/src/com/newsblur/fragment/SettingsFragment.java
@@ -17,13 +17,6 @@ public class SettingsFragment extends PreferenceFragment {
         PreferenceManager preferenceManager = getPreferenceManager();
         preferenceManager.setSharedPreferencesName(PrefConstants.PREFERENCES);
         addPreferencesFromResource(R.xml.activity_settings);
-
-        // Remove the reading category of references on pre-4.4 devices as it only contains
-        // the single tap for immersive preference
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            PreferenceCategory readingCategory = (PreferenceCategory)findPreference("reading");
-            getPreferenceScreen().removePreference(readingCategory);
-        }
     }
 
 }
