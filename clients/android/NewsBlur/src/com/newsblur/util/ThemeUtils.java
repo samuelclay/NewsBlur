830782011b02262082afc9b458d94c4012de03d1
dosiecki <ojiikun@gmail.com>
2017-10-03 21:43:14 -0700

    migrate some hardcoded theme logic to use theme engine

--- clients/android/NewsBlur/src/com/newsblur/util/ThemeUtils.java
+++ clients/android/NewsBlur/src/com/newsblur/util/ThemeUtils.java
@@ -11,39 +11,6 @@ public class ThemeUtils {
 
     private ThemeUtils() {} // util class - no instances
 
-    // Resources.getColor(rid) was deprecated for a version that actually has a native
-    // understanding of themes, but isn't available unless platform version > Build.M.
-    // If we ever bump min platform version to M or higher, we could greatly simplify
-    // or totally remove this whole utility class.
-    @SuppressWarnings("deprecation")
-    private static int getColor(Context context, int id) {
-        return context.getResources().getColor(id);
-    }
-
-    public static int getProfileActivitiesLinkColor(Context context) {
-        if (PrefsUtils.isLightThemeSelected(context)) {
-            return getColor(context, R.color.linkblue);
-        } else {
-            return getColor(context, R.color.dark_linkblue);
-        }
-    }
-
-    public static int getProfileActivitiesContentColor(Context context) {
-        if (PrefsUtils.isLightThemeSelected(context)) {
-            return getColor(context, R.color.text);
-        } else {
-            return getColor(context, R.color.white);
-        }
-    }
-
-    public static int getProfileActivitiesQuoteColor(Context context) {
-        if (PrefsUtils.isLightThemeSelected(context)) {
-            return getColor(context, R.color.gray55);
-        } else {
-            return getColor(context, R.color.gray80);
-        }
-    }
-
     public static int getSelectorOverlayBackgroundText(Context context) {
         if (PrefsUtils.isLightThemeSelected(context)) {
             return R.drawable.selector_overlay_bg_text;
