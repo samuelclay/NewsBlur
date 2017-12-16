2dad2f374cb0263ba0db53c3f5ff22f2e3dd8f93
dosiecki <ojiikun@gmail.com>
2017-12-15 16:32:01 -0800

    swap infrequent and all rows

--- clients/android/NewsBlur/src/com/newsblur/database/FolderListAdapter.java
+++ clients/android/NewsBlur/src/com/newsblur/database/FolderListAdapter.java
@@ -582,8 +582,8 @@ public class FolderListAdapter extends BaseExpandableListAdapter {
         // add the always-present (if enabled) special rows/folders that got at the top of the list
         if (PrefsUtils.isEnableRowGlobalShared(context) && (currentState != StateFilter.SAVED)) addSpecialRow(GLOBAL_SHARED_STORIES_GROUP_KEY);
         if ((currentState != StateFilter.SAVED)) addSpecialRow(ALL_SHARED_STORIES_GROUP_KEY);
-        addSpecialRow(ALL_STORIES_GROUP_KEY);
         if (PrefsUtils.isEnableRowInfrequent(context) && (currentState != StateFilter.SAVED)) addSpecialRow(INFREQUENT_SITE_STORIES_GROUP_KEY);
+        addSpecialRow(ALL_STORIES_GROUP_KEY);
         // create a sorted list of folder display names
         List<String> sortedFolderNames = new ArrayList<String>(flatFolders.keySet());
         Collections.sort(sortedFolderNames, Folder.FolderNameComparator);
