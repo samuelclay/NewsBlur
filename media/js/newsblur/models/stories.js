d846c35ae7382e92efde45d26f5679b82ed9343f
Samuel Clay <samuel@ofbrooklyn.com>
2018-07-24 15:59:21 -0400

    Wrong element on story title view.

--- media/js/newsblur/models/stories.js
+++ media/js/newsblur/models/stories.js
@@ -165,7 +165,7 @@ NEWSBLUR.Models.Story = Backbone.Model.extend({
             });
             var success = !this.story_title_view.$st.find('a')[0].dispatchEvent(event);
             if (!success) {
-                success = this.story_title_view.hasClass('NB-story-webkit-opened');
+                success = this.story_title_view.$st.hasClass('NB-story-webkit-opened');
             }
             if (success) {
                 // console.log(['Used safari extension to open link in background', success]);
