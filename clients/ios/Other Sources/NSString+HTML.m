3d61e34e38f95143bec38ff21aabb44168d5ea11
David Sinclair <dev@dejal.com>
2019-02-25 20:42:05 -0800

    iOS: fixed #1169 (scroll performance)
    Improved the performance of the convertHTML method, used when calculating dynamic row heights.

--- clients/ios/Other Sources/NSString+HTML.m
+++ clients/ios/Other Sources/NSString+HTML.m
@@ -35,20 +35,21 @@
 #pragma mark - Instance Methods
 
 - (NSString *)convertHTML {
-    NSScanner *myScanner;
-    NSString *text = nil;
-    NSString *html = self;
-    myScanner = [NSScanner scannerWithString:html];
+    NSScanner *scanner = [NSScanner scannerWithString:self];
+    NSMutableString *output = [NSMutableString string];
+    NSString *string = nil;
     
-    while ([myScanner isAtEnd] == NO) {
-        [myScanner scanUpToString:@"<" intoString:NULL] ;
-        [myScanner scanUpToString:@">" intoString:&text] ;
-        html = [html stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@>", text] withString:@""];
+    while ([scanner isAtEnd] == NO) {
+        [scanner scanUpToString:@"<" intoString:&string];
+        [scanner scanUpToString:@">" intoString:NULL];
+        [scanner scanString:@">" intoString:NULL];
+        
+        if (string != nil) {
+            [output appendString:string];
+        }
     }
-
-    html = [html stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     
-    return html;
+    return [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 }
 
 - (NSString *)stringByDecodingXMLEntities {
