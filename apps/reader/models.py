a66e9e2c09353d68aa97485d582a926bbe3f7797
Samuel Clay <samuel@ofbrooklyn.com>
2019-05-27 14:41:01 -0400

    Fixing issue where deleting feeds would re-subscribe a user down the line because the feed had a duplicate feed id subscribed but the original would get added, so deleting it would not delete the duplicate. Now rewriting duplicate feed ids to match the new subscribed feed.

--- apps/reader/models.py
+++ apps/reader/models.py
@@ -1315,7 +1315,7 @@ class UserSubscriptionFolders(models.Model):
         new_folders = _compact(folders)
         logging.info(" ---> Compacting from %s to %s" % (folders, new_folders))
         new_folders = json.encode(new_folders)
-        logging.info(" ---> Compacting from %s to %s" % (len(self.folders), len(new_folders)))
+        logging.info(" ---> Compacting from %s bytes to %s bytes" % (len(self.folders), len(new_folders)))
         self.folders = new_folders
         self.save()
         
@@ -1623,6 +1623,11 @@ class UserSubscriptionFolders(models.Model):
                           self.user, len(missing_subs), missing_subs))
             for feed_id in missing_subs:
                 feed = Feed.get_by_id(feed_id)
+                if feed_id != feed.pk:
+                    # Clear out duplicate sub in folders before subscribing to feed
+                    duplicate_feed = Feed.get_by_id(feed_id)
+                    duplicate_feed.pk = feed_id
+                    self.rewrite_feed(feed, duplicate_feed)
                 if feed:
                     us, _ = UserSubscription.objects.get_or_create(user=self.user, feed=feed, defaults={
                         'needs_unread_recalc': True
