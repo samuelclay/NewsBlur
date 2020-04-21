168cdb2ea91838c843acede836e888304801dbb8
David Sinclair <dev@dejal.com>
2020-04-21 16:05:31 -0700

    #1301 (white flash in dark mode)
    - Added a tiny delay when loading the full story HTML to give the web view time to render the page.

--- clients/ios/Classes/StoryDetailViewController.m
+++ clients/ios/Classes/StoryDetailViewController.m
@@ -1810,8 +1810,10 @@
                        });
     }
     
-    self.webView.hidden = NO;
-    [self.webView setNeedsDisplay];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.webView.hidden = NO;
+        [self.webView setNeedsDisplay];
+    });
 }
 
 - (void)webViewNotifyLoaded {
