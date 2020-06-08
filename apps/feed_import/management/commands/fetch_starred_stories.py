ebd0da2d63c3d6461f8eaedfaa0c029353af0dda
jmath1 <jonathanamath@gmail.com>
2020-06-08 07:55:17 -0400

    change management commands to use argparse instead of optparse

--- apps/feed_import/management/commands/fetch_starred_stories.py
+++ apps/feed_import/management/commands/fetch_starred_stories.py
@@ -2,18 +2,38 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.auth.models import User
 from apps.feed_import.models import GoogleReaderImporter
-from optparse import make_option
 from utils.management_functions import daemonize
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option("-d", "--daemon", dest="daemonize", action="store_true"),
-        make_option('-u', '--username', type='str', dest='username'),
-        make_option('-c', '--count', type='int', dest='count', default=1000),
-        make_option('-V', '--verbose', action='store_true',
-            dest='verbose', default=False, help='Verbose output.'),
-    )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--daemon',
+            '-d',
+            dest='daemonize',
+            action="store_true"
+        )
+        parser.add_argument(
+            '--username',
+            '-u'
+            type=str,
+            dest='username'
+        )
+        parser.add_argument(
+            '--count',
+            '-c'
+            type=int,
+            dest='count',
+            default=1000
+        )
+        parser.add_argument(
+            '--verbose',
+            '-V',
+            action='store_true',
+            dest='verbose',
+            default=False,
+            help='Verbose output.'
+        )
 
     def handle(self, *args, **options):
         if options['daemonize']:
--- apps/profile/management/commands/reimport_stripe_history.py
+++ apps/profile/management/commands/reimport_stripe_history.py
@@ -2,19 +2,16 @@ import stripe, datetime, time
 from django.conf import settings
 
 from django.core.management.base import BaseCommand
-from optparse import make_option
 
 from utils import log as logging
 from apps.profile.models import Profile
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        # make_option("-u", "--username", dest="username", nargs=1, help="Specify user id or username"),
-        # make_option("-e", "--email", dest="email", nargs=1, help="Specify email if it doesn't exist"),
-        make_option("-d", "--days", dest="days", nargs=1, type='int', default=365, help="Number of days to go back"),
-        make_option("-l", "--limit", dest="limit", nargs=1, type='int', default=100, help="Charges per batch"),
-        make_option("-s", "--start", dest="start", nargs=1, type='string', default=None, help="Offset customer_id (starting_after)"),
-    )
+
+    def add_arguments(self, parser)
+        parser.add_argument("-d", "--days", dest="days", nargs=1, type='int', default=365, help="Number of days to go back")
+        parser.add_argument("-l", "--limit", dest="limit", nargs=1, type='int', default=100, help="Charges per batch")
+        parser.add_argument("-s", "--start", dest="start", nargs=1, type='string', default=None, help="Offset customer_id (starting_after)")
 
     def handle(self, *args, **options):
         limit = options.get('limit')
