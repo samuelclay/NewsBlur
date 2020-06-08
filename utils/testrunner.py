bcfc17a9dbf59ce4de06eac70a96f14ead77fb3e
jmath1 <jonathanamath@gmail.com>
2020-06-08 07:56:11 -0400

    use DiscoverRunner to run tests

--- utils/testrunner.py
+++ utils/testrunner.py
@@ -1,8 +1,7 @@
-from django.test.simple import DjangoTestSuiteRunner
-
+from django.test.runner import DiscoverRunner
 from mongoengine import connect
 
-class TestRunner(DjangoTestSuiteRunner):
+class TestRunner(DiscoverRunner):
     def setup_databases(self, **kwargs):
         db_name = 'newsblur_test'
         connect(db_name)
