ccff80d2cc90f7d84edea38c7f5b82eed13fe6ea
jmath1 <jonathanamath@gmail.com>
2020-06-11 03:50:44 -0400

    Update logging class in settings.py to logging.NullHandler

--- settings.py
+++ settings.py
@@ -162,7 +162,7 @@ LOGGING = {
     'handlers': {
         'null': {
             'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
+            'class':'logging.NullHandler',
         },
         'console':{
             'level': 'DEBUG',
