d60ec99f3e38f769c96bb9f8c0d18dee1158d9d6
Samuel Clay <samuel@ofbrooklyn.com>
2020-06-15 17:53:35 -0400

    Removing additional traces of Google Reader.

--- apps/feed_import/tasks.py
+++ apps/feed_import/tasks.py
@@ -1,6 +1,6 @@
 from celery.task import Task
 from django.contrib.auth.models import User
-from apps.feed_import.models import UploadedOPML, OPMLImporter, GoogleReaderImporter
+from apps.feed_import.models import UploadedOPML, OPMLImporter
 from apps.reader.models import UserSubscription
 from apps.rss_feeds.models import MStarredStory
 from utils import log as logging
@@ -19,34 +19,4 @@ class ProcessOPML(Task):
         feed_count = UserSubscription.objects.filter(user=user).count()
         user.profile.send_upload_opml_finished_email(feed_count)
         logging.user(user, "~FR~SBOPML upload (task): ~SK%s~SN~SB~FR feeds" % (feed_count))
-
-
-class ProcessReaderImport(Task):
-    
-    def run(self, user_id, auto_active=False):
-        user = User.objects.get(pk=user_id)
-        logging.user(user, "~FR~SBGoogle Reader import (task) starting...")
-
-        importer = GoogleReaderImporter(user=user)
-        importer.import_feeds(auto_active=auto_active)
-        importer.import_starred_items(count=10)
-        
-        feed_count = UserSubscription.objects.filter(user=user).count()
-        user.profile.send_import_reader_finished_email(feed_count)
-        logging.user(user, "~FR~SBGoogle Reader import (task): ~SK%s~SN~SB~FR feeds" % (feed_count))
-
-
-class ProcessReaderStarredImport(Task):
-    
-    def run(self, user_id):
-        user = User.objects.get(pk=user_id)
-        logging.user(user, "~FR~SBGoogle Reader starred stories import (task) starting...")
-
-        importer = GoogleReaderImporter(user=user)
-        importer.import_starred_items(count=1000)
-        
-        feed_count = UserSubscription.objects.filter(user=user).count()
-        starred_count = MStarredStory.objects.filter(user_id=user.pk).count()
-        user.profile.send_import_reader_starred_finished_email(feed_count, starred_count)
-        logging.user(user, "~FR~SBGoogle Reader starred stories import (task): ~SK%s~SN~SB~FR feeds, ~SK%s~SN~SB~FR starred stories" % (feed_count, starred_count))
 
--- fabfile.py
+++ fabfile.py
@@ -468,7 +468,7 @@ def setup_virtualenv():
                 # sudo('rmvirtualenv newsblur')
                 # sudo('rm -fr venv')
                 with settings(warn_only=True):
-                    run('mkvirtualenv --no-site-packages newsblur')
+                    run('mkvirtualenv newsblur')
                 run('echo "import sys; sys.setdefaultencoding(\'utf-8\')" | sudo tee venv/newsblur/lib/python2.7/sitecustomize.py')
                 run('echo "/srv/newsblur" | sudo tee venv/newsblur/lib/python2.7/site-packages/newsblur.pth')
     
@@ -761,8 +761,8 @@ def setup_staging():
         run('touch logs/newsblur.log')
 
 def setup_node_app():
-    sudo('curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -')
-    sudo('apt-get install -y nodejs')
+    sudo('curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -')
+    sudo('apt-get install -y nodejs npm')
     # run('curl -L https://npmjs.org/install.sh | sudo sh')
     # sudo('apt-get install npm')
     sudo('sudo npm install -g npm')
