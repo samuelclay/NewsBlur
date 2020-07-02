1e5474a9ecabe6aeb5d0b6d7205d2bcfcbf11f54
Samuel Clay <samuel@ofbrooklyn.com>
2020-07-02 00:07:20 -0400

    Adding cookie logging

--- utils/request_introspection_middleware.py
+++ utils/request_introspection_middleware.py
@@ -7,7 +7,7 @@ class DumpRequestMiddleware:
             request_data = request.POST or request.GET
             request_items = list(request_data.items())
             if request_items:
-                logging.debug(" ---> ~FC%s ~SN~FC%s ~SN~BC~FK%s~BK~FC" % (request.method, request.path, dict(request_items)))
+                logging.debug(" ---> ~FC%s ~SN~FC%s ~SN~BC~FK%s~BK~FC %s" % (request.method, request.path, dict(request_items), request.cookies))
 
     def __init__(self, get_response=None):
         self.get_response = get_response
