54dfbe9c415fb9e83b88d870cefa476ee095fb97
Jonathan Math <jonathanamath@gmail.com>
2020-10-24 19:43:17 +0700

    change redis port to 6579 when DOCKERBUILD is an env variable

--- node/unread_counts.js
+++ node/unread_counts.js
@@ -14,6 +14,8 @@
 
   REDIS_SERVER = process.env.NODE_ENV === 'development' ? 'localhost' : DOCKER ? 'redis' : 'db_redis_pubsub';
   
+  REDIS_PORT = process.env.NODE_ENV === 'docker' ? 6379 : 6579;
+  
   SECURE = !!process.env.NODE_SSL;
 
   log.debug("Starting NewsBlur unread count server...");
@@ -70,7 +72,7 @@
       if ((_ref = socket.subscribe) != null) {
         _ref.quit();
       }
-      socket.subscribe = redis.createClient(6379, REDIS_SERVER);
+      socket.subscribe = redis.createClient(REDIS_PORT, REDIS_SERVER);
       socket.subscribe.on("error", (function(_this) {
         return function(err) {
           var _ref1;
