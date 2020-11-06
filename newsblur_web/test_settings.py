778ee2123fc9f3a3ceb991df91aa789e427c6e15
Jonathan Math <jonathanamath@gmail.com>
2020-11-06 14:47:41 +0700

    use pytest and add pytest.ini file

--- newsblur_web/test_settings.py
+++ newsblur_web/test_settings.py
@@ -3,11 +3,10 @@ from newsblur_web.settings import *
 
 DOCKERBUILD = os.getenv("DOCKERBUILD")
 
-if 'test' in sys.argv:
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-    DATABASES['default']['OPTIONS'] = {}
-    DATABASES['default']['NAME'] = 'nb.db'
-    DATABASES['default']['TEST_NAME'] = 'nb2.db'
+DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+DATABASES['default']['OPTIONS'] = {}
+DATABASES['default']['NAME'] = 'nb.db'
+DATABASES['default']['TEST_NAME'] = 'nb2.db'
     
 # DATABASES = {
 #     'default':{
@@ -18,13 +17,18 @@ if 'test' in sys.argv:
 # }
 if DOCKERBUILD:
     MONGO_PORT = 29019
+    MONGO_DB = {
+        'name': 'newsblur_test',
+        'host': '127.0.0.1:29019',
+    }
+
 else:
     MONGO_PORT = 27017
-
-MONGO_DB = {
-    'name': 'newsblur_test',
-    'host': '127.0.0.1:29019',
-}
+    MONGO_DB = {
+        'name': 'newsblur_test',
+        'host': '127.0.0.1:27017',
+    }
+    SERVER_NAME
 
 MONGO_DATABASE_NAME = 'test_newsblur'
 # TEST_DATABASE_NAME = ":memory:"
@@ -36,6 +40,7 @@ DEBUG = True
 SITE_ID = 2
 RAVEN_CLIENT = None
 HOMEPAGE_USERNAME = 'conesus'
+SERVER_NAME = 'test_newsblur'
 # from django.db import connection
 # cursor = connection.cursor()
 # cursor.execute('PRAGMA temp_store = MEMORY;')
