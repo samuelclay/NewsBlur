2338555eb74a47bb1fe797d9e682a40bd38f7dde
Samuel Clay <samuel@ofbrooklyn.com>
2020-11-10 12:53:03 -0500

    Whoops, left detritus from stash.

--- utils/view_functions.py
+++ utils/view_functions.py
@@ -94,8 +94,4 @@ class required_params(object):
         return HttpResponse(json.encode({
             'message': message,
             'code': -1,
-<<<<<<< Updated upstream
         }), content_type="application/json", status=status_code)
-=======
-        }), mimetype="application/json", status=status_code)
->>>>>>> Stashed changes
