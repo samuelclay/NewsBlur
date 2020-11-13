1c622661274dce9d1da9537564c700d353f564ba
Samuel Clay <samuel@ofbrooklyn.com>
2020-11-13 11:10:03 -0500

    mimetype -> content_type

--- apps/api/views.py
+++ apps/api/views.py
@@ -158,7 +158,7 @@ def add_site(request, token):
         'code':    code,
         'message': message,
         'usersub': us and us.feed_id,
-    }) + ')', mimetype='text/plain')
+    }) + ')', content_type='text/plain')
 
 @ajax_login_required
 def add_site_authed(request):
@@ -193,7 +193,7 @@ def add_site_authed(request):
         'code':    code,
         'message': message,
         'usersub': us and us.feed_id,
-    }) + ')', mimetype='text/plain')
+    }) + ')', content_type='text/plain')
 
 def check_share_on_site(request, token):
     code       = 0
@@ -274,7 +274,7 @@ def check_share_on_site(request, token):
         'other_stories'     : other_stories,
         'previous_stories'  : previous_stories,
         'users'             : users,
-    }) + ')', mimetype='text/plain')
+    }) + ')', content_type='text/plain')
     response['Access-Control-Allow-Origin'] = '*'
     response['Access-Control-Allow-Methods'] = 'GET'
     
@@ -310,7 +310,7 @@ def share_story(request, token=None):
             'code':     code,
             'message':  message,
             'story':    None,
-        }), mimetype='text/plain')
+        }), content_type='text/plain')
     
     if feed_id:
         feed = Feed.get_by_id(feed_id)
@@ -389,7 +389,7 @@ def share_story(request, token=None):
         'code':     code,
         'message':  message,
         'story':    shared_story,
-    }), mimetype='text/plain')
+    }), content_type='text/plain')
     response['Access-Control-Allow-Origin'] = '*'
     response['Access-Control-Allow-Methods'] = 'POST'
     
@@ -427,7 +427,7 @@ def save_story(request, token=None):
             'code':     code,
             'message':  message,
             'story':    None,
-        }), mimetype='text/plain')
+        }), content_type='text/plain')
     
     if feed_id:
         feed = Feed.get_by_id(feed_id)
@@ -492,7 +492,7 @@ def save_story(request, token=None):
         'code':     code,
         'message':  message,
         'story':    starred_story,
-    }), mimetype='text/plain')
+    }), content_type='text/plain')
     response['Access-Control-Allow-Origin'] = '*'
     response['Access-Control-Allow-Methods'] = 'POST'
     
