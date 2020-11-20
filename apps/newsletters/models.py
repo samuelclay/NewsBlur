f15a1068f2e4e1328818c277ce556a3d4d810a50
Samuel Clay <samuel@ofbrooklyn.com>
2020-11-19 19:59:07 -0500

    Handling multiple newsletter feeds.

--- apps/newsletters/models.py
+++ apps/newsletters/models.py
@@ -34,6 +34,10 @@ class EmailNewsletter:
         # First look for the email address
         try:
             feed = Feed.objects.get(feed_address=feed_address)
+        except Feed.MultipleObjectsReturned:
+            feeds = Feed.objects.filter(feed_address=feed_address).limit(1)
+            if feeds.count():
+                feed = feeds[0]
         except Feed.DoesNotExist:
             feed = None
 
