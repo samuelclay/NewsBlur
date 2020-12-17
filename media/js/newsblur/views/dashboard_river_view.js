163578646ddd404627fe46515aa232f69681b023
Samuel Clay <samuel@ofbrooklyn.com>
2020-12-17 09:18:11 -0500

    Turning on refresh in debug for dashboard.

--- media/js/newsblur/views/dashboard_river_view.js
+++ media/js/newsblur/views/dashboard_river_view.js
@@ -102,7 +102,7 @@ NEWSBLUR.Views.DashboardRiver = Backbone.View.extend({
     // ===========
     
     setup_dashboard_refresh: function() {
-        if (NEWSBLUR.Globals.debug) return;
+        // if (NEWSBLUR.Globals.debug) return;
         
         // Reload dashboard graphs every N minutes.
         var reload_interval = NEWSBLUR.Globals.is_staff ? 60*1000 : 15*60*1000;
