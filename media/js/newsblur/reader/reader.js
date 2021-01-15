eca5468c4ceacbbea57e207ea1e7129cf98eea58
Samuel Clay <samuel@ofbrooklyn.com>
2021-01-14 20:15:51 -0500

    Also rerendering story titles on main app.

--- media/js/newsblur/reader/reader.js
+++ media/js/newsblur/reader/reader.js
@@ -3059,6 +3059,7 @@
                 this.show_story_titles_above_intelligence_level({ 'animate': true, 'follow': true });
                 NEWSBLUR.app.dashboard_rivers.left.redraw();
                 NEWSBLUR.app.dashboard_rivers.right.redraw();
+                NEWSBLUR.app.story_titles.render();
             }
         },
         
