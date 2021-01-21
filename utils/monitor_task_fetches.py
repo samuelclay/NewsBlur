e15c5a358f1c238ff19f3ab97182e1dceb0c5723
Samuel Clay <samuel@ofbrooklyn.com>
2021-01-20 21:25:01 -0500

    Handling work queue missing error.

--- utils/monitor_task_fetches.py
+++ utils/monitor_task_fetches.py
@@ -41,7 +41,7 @@ def main():
                 data={"from": "NewsBlur Task Monitor: %s <admin@%s.newsblur.com>" % (hostname, hostname),
                       "to": [admin_email],
                       "subject": "%s feeds fetched falling: %s (from %s)" % (hostname, feeds_fetched, redis_task_fetches),
-                      "text": "Feed fetches are falling: %s (from %s)" % (feeds_fetched, redis_task_fetches)})
+                      "text": "Feed fetches are falling: %s (from %s) %s" % (feeds_fetched, redis_task_fetches, failed)})
 
         r.set(monitor_key, feeds_fetched)
         r.expire(monitor_key, 60*60*3) # 3 hours
