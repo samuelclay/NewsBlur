bc1eec31967cbb10f59a387f3adc4c66f280a07e
Jonathan Math <jonathanamath@gmail.com>
2021-02-08 14:17:35 -0500

    remove ansible/digitalocean.py and provision.py as they are no longer used

--- ansible/provision.py
+++
@@ -1,7 +0,0 @@
-import os
-from glob import glob
-import subprocess
-playbooks = glob('*setup_*.yml')
-playbooks.remove('setup_www.yml')
-os.chdir('..')
-[subprocess.call(f'ansible-playbook ansible/{playbook}', shell=True) for playbook in playbooks ]
