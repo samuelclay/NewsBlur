de324f58e29ac7d7009b0e43ecece121efc9f4c6
Samuel Clay <samuel@ofbrooklyn.com>
2021-02-10 18:38:38 -0500

    Writing consul-manager IP to /srv/newsblur/consul/consul_manager_ip.txt

--- ansible/roles/consul/tasks/get_consul_manager_ip.py
+++ ansible/roles/consul/tasks/get_consul_manager_ip.py
@@ -1,4 +1,4 @@
-
+#!/srv/newsblur/venv/newsblur3/bin/python
 import os
 import digitalocean
 
@@ -13,7 +13,9 @@ my_droplets = manager.get_all_droplets()
 consul_manager_droplet = [d for d in my_droplets if d.name == "consul-manager"][0]
 consul_manager_ip_address = consul_manager_droplet.ip_address
 
-# write or overwrite the consul-manager ip
-if "consul_manager_ip.txt" not in os.listdir('/srv/newsblur/consul/'):
-    with open('/srv/newsblur/consul/consul_manager_ip.txt', 'w') as f:
-       f.write(consul_manager_ip_address)
+print(consul_manager_ip_address)
+
+# # write or overwrite the consul-manager ip
+# if "consul_manager_ip.txt" not in os.listdir('/srv/newsblur/consul/'):
+#     with open('/srv/newsblur/consul/consul_manager_ip.txt', 'w') as f:
+#        f.write(consul_manager_ip_address)
