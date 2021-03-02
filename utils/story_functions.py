94e27a1f7bd35f67f950012befa3b7c13a9de07c
Samuel Clay <samuel@ofbrooklyn.com>
2021-03-02 18:33:00 -0500

    No need to decode

--- utils/story_functions.py
+++ utils/story_functions.py
@@ -277,7 +277,7 @@ def truncate_chars(value, max_length):
         return value.decode('utf-8', 'ignore')
  
     truncd_val = value[:max_length]
-    if value[max_length].decode() != " ":
+    if value[max_length] != b" ":
         rightmost_space = truncd_val.rfind(b" ")
         if rightmost_space != -1:
             truncd_val = truncd_val[:rightmost_space]
