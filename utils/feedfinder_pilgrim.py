213ee843f26cb0c23a2e4e11541229fbc8e43ede
Samuel Clay <samuel@ofbrooklyn.com>
2021-03-24 20:30:11 -0400

    Fixing feed finder py3 issue.

--- utils/feedfinder_pilgrim.py
+++ utils/feedfinder_pilgrim.py
@@ -207,7 +207,7 @@ def isXMLRelatedLink(link):
 
 r_brokenRedirect = re.compile('<newLocation[^>]*>(.*?)</newLocation>', re.S)
 def tryBrokenRedirect(data):
-    if '<newLocation' in data:
+    if b'<newLocation' in data:
         newuris = r_brokenRedirect.findall(data)
         if newuris and newuris[0]: return newuris[0].strip()
 
