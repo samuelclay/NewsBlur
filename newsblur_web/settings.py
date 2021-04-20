0235a907dab623999bf5147916102a62aa6db709
Samuel Clay <samuel@ofbrooklyn.com>
2021-04-20 19:24:50 -0400

    Let's try reducing celery worker counts and seeing if that boosts work performed.

--- newsblur_web/settings.py
+++ newsblur_web/settings.py
@@ -414,7 +414,7 @@ CELERY_IMPORTS              = ("apps.rss_feeds.tasks",
                                "apps.feed_import.tasks",
                                "apps.search.tasks",
                                "apps.statistics.tasks",)
-CELERY_WORKER_CONCURRENCY         = 4
+CELERY_WORKER_CONCURRENCY         = 3
 CELERY_TASK_IGNORE_RESULT        = True
 CELERY_TASK_ACKS_LATE            = True # Retry if task fails
 CELERY_WORKER_MAX_TASKS_PER_CHILD = 10
