79a1016ab078371a8113f2de54703234fec88b0d
David Sinclair <dev@dejal.com>
2021-05-07 21:54:34 -0700

    #1466 (stories aren't always rendering)
    - A better fix; I’m pretty sure this fixes it.

--- clients/ios/Classes/StoryDetailObjCViewController.m
+++ clients/ios/Classes/StoryDetailObjCViewController.m
@@ -632,11 +632,14 @@
     
     dispatch_async(dispatch_get_main_queue(), ^{
 //        NSLog(@"Drawing Story: %@", [self.activeStory objectForKey:@"story_title"]);
+        if (self.hasStory)
+            return;
+        
         [self loadHTMLString:htmlTopAndBottom];
         [self.appDelegate.storyPagesViewController setTextButton:(StoryDetailViewController *)self];
     });
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self loadStory];
     });
     
