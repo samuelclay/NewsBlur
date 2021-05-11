5ebcd0d324fadeeb4982433b98cc79648d9431e1
Matt Payne <matthew.payne.15@cnu.edu>
2021-05-11 17:18:29 -0400

    Update constants.py

--- apps/search/constants.py
+++ apps/search/constants.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 SPARSE_FEATURES = ["feed_id", "user", "active", "is_premium"]
 
-DENSE_FEATURES = ["premium_subs", "active_subs", "num_subs", "read_pct", "reader_count", "reach_score", "story_count", "share_count", "average_stories_per_month", 'active_premium_subscribers']
+DENSE_FEATURES = ["premium_subs", "active_subs", "num_subs", "read_pct", "reader_count", "reach_score", "story_count", "share_count", "average_stories_per_month", 'active_premium_subscribers','user_shared_stories_count']
 
 TARGET = ['is_following_feed']
 # need to save vocabulary sizes to use in creating features
