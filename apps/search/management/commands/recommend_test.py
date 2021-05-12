0f76fdaef87c44ba5c355ced5dcec5337f1f664b
Matt Payne <matthew.payne.15@cnu.edu>
2021-05-11 21:02:16 -0400

    Update recommend_test.py

--- apps/search/management/commands/recommend_test.py
+++ apps/search/management/commands/recommend_test.py
@@ -107,6 +107,7 @@ class Command(BaseCommand):
         for feat in SPARSE_FEATURES:
                 # need a labelEncoder for each feature
                 lbe = load(open( feat + '-' + 'lbe.pkl', 'rb'))
+                print('loaded ' + feat)
                 input_df[feat] = lbe.transform(input_df[feat])
                 vocabs[feat] = len(lbe.classes_)
 
