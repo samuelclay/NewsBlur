8ede5a3987997c5181a94ddba9619d65d36b7294
Jonathan Math <jonathanamath@gmail.com>
2021-06-27 14:11:14 -0600

    change get_postgres_credentials.py to get_credentials.py so it can also be used to get the s3 bucket name. Fix postgres backups

--- ansible/roles/postgres-exporter/tasks/get_credentials.py
+++ ansible/roles/postgres-exporter/tasks/get_credentials.py
@@ -5,4 +5,8 @@ from newsblur_web import settings
 
 username = settings.DATABASES['default']['USER']
 password = settings.DATABASES['default']['PASSWORD']
-print(f"{username}:{password}")
+
+if sys.argv[1] =='postgres_credentials':
+    print(f"{username}:{password}")
+if sys.argv[1] =='s3_bucket':
+    print(settings.S3_BACKUP_BUCKET)
--- utils/backups/backup_psql.py
+++ utils/backups/backup_psql.py
@@ -4,24 +4,15 @@ import sys
 
 NEWSBLUR_DIR = '/srv/newsblur'
 sys.path.insert(0, NEWSBLUR_DIR)
-VENDOR_DIR = '/srv/newsblur/vendor'
-sys.path.insert(0, VENDOR_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'newsblur_web.settings'
 
-import time
+
 import boto3
-from django.conf import settings
 
-db_name = 'newsblur'
-db_pass = settings.DATABASES['default']['PASSWORD']
-filename = '/backup/backup_postgresql_%s.sql.gz' % time.strftime('%Y-%m-%d-%H-%M')
-cmd      = f'docker exec -it postgres export PGPASSWORD="{db_pass}"; /usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc {db_name} > {filename}'
-print('Backing up PostgreSQL: %s' % cmd)
-os.system(cmd)
+filename = sys.argv[1]
 
 print('Uploading %s to S3...' % filename)
 
 s3 = boto3.resource('s3') 
-bucket = s3.Bucket(settings.get('S3_BACKUP_BUCKET'))
-bucket.upload_file(filename, name="postgres/%s" % filename)
+bucket = sys.argv[2]
+bucket.upload_file(filename, name="postgres/%s" % filename.split("/")[1])
 os.remove(filename)
