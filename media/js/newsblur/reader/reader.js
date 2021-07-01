e68f58f80a7ee137dd77167d88e877d70c02ae87
Samuel Clay <samuel@ofbrooklyn.com>
2021-06-30 20:43:12 -0400

    25% www -> beta.

--- media/js/newsblur/reader/reader.js
+++ media/js/newsblur/reader/reader.js
@@ -171,7 +171,7 @@
 
         check_if_forced_upgrade_to_beta_site: function () {
             if (
-                NEWSBLUR.Globals.user_id % 100 <= 10
+                NEWSBLUR.Globals.user_id % 100 <= 25 && !NEWSBLUR.Globals.is_admin
             ) {
                 // See ya
                 window.location.href = "https://beta.newsblur.com";
