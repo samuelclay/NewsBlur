ad9b805d0a3176b95bbf5ebf7eb8acf8457434c6
Samuel Clay <samuel@ofbrooklyn.com>
2021-07-23 19:04:30 -0400

    Updating backup to use boto3

--- docker/mongo/backup_mongo.sh
+++ docker/mongo/backup_mongo.sh
@@ -30,6 +30,7 @@ tar -zcf /opt/mongo/newsblur/backup/backup_mongo_${now}.tgz /opt/mongo/newsblur/
 echo " ---> Uploading backups to S3"
 docker run --rm -v /srv/newsblur:/srv/newsblur -v /opt/mongo/newsblur/backup/:/opt/mongo/newsblur/backup/ --network=newsblurnet newsblur/newsblur_python3:latest python /srv/newsblur/utils/backups/backup_mongo.py
 
-rm /opt/mongo/newsblur/backup/backup_mongo_${now}.tgz
-rm /opt/mongo/newsblur/backup/backup_mongo_${now}
+# Don't delete backup since the backup_mongo.py script will rm them
+## rm /opt/mongo/newsblur/backup/backup_mongo_${now}.tgz
+## rm /opt/mongo/newsblur/backup/backup_mongo_${now}
 echo " ---> Finished uploading backups to S3: backup_mongo_${now}.tgz"
--- utils/backups/backup_mongo.py
+++ utils/backups/backup_mongo.py
@@ -10,9 +10,9 @@ filenames = [f for f in os.listdir('/opt/mongo/newsblur/backup/') if '.tgz' in f
 for filename in filenames:
     print('Uploading %s to S3...' % filename)
     try:
-        s3 = boto3.resource('s3') 
-        bucket = s3.Bucket(settings.S3_BACKUP_BUCKET)
-        bucket.upload_file(filename, name="mongo/%s" % (filename))
+        s3 = boto3.client('s3') 
+        bucket = s3.Bucket()
+        bucket.upload_file(f"mongo/{filename}", settings.S3_BACKUP_BUCKET)
     except Exception as e:
         print(" ****> Exceptions: %s" % e)
     shutil.rmtree(filename[:-4])
