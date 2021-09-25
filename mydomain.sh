686009a6a352f14cd354907116a515b477ad3176
Linecutterx <40548573+Linecutterx@users.noreply.github.com>
2021-09-25 23:28:48 +0100

    Update mydomain.sh
    Change substitution to acually be localhost!

--- mydomain.sh
+++ mydomain.sh
@@ -6,10 +6,10 @@ if [ $# -eq 0 ]
     if [ $# -eq 1 ]
      then
      echo "$(tput setaf 1)Changinging newsblur_web/docker_local_settings.py:$(tput setaf 2) localhost to $1$(tput sgr 0)"
-     sed -i'.bak'  -e "/NEWSBLUR_URL/ s/nb\.macklin\.co/$1/" -e "/NEWSBLUR_SESSION_COOKIE/ s/localhost/$1/" './newsblur_web/docker_local_settings.py'
+     sed -i'.bak'  -e "/NEWSBLUR_URL/ s/localhost/$1/" -e "/NEWSBLUR_SESSION_COOKIE/ s/localhost/$1/" './newsblur_web/docker_local_settings.py'
      head -20 './newsblur_web/docker_local_settings.py'
      echo "$(tput setaf 1)Changinging config/fixtures/bootstrap.json:$(tput setaf 2) localhost to $1$(tput sgr 0)"
-     sed -i'.bak' "/domain/ s/nb\.macklin\.co/$1/" './config/fixtures/bootstrap.json'
+     sed -i'.bak' "/domain/ s/localhost/$1/" './config/fixtures/bootstrap.json'
      head -10 './config/fixtures/bootstrap.json'
     else
       if [ $# -eq 2 ]
