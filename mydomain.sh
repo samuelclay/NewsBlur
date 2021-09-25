e732adb36dc2ab10c02b3eacd0cbfea086a1b537
Linecutterx <40548573+Linecutterx@users.noreply.github.com>
2021-09-26 00:01:30 +0100

    Update mydomain.sh

--- mydomain.sh
+++ mydomain.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]
     if [ $# -eq 1 ]
      then
      echo "$(tput setaf 1)Changinging newsblur_web/docker_local_settings.py:$(tput setaf 2) localhost to $1$(tput sgr 0)"
-     sed -i'.bak'  -e "/NEWSBLUR_URL/ s/localhost/$1/" -e "/NEWSBLUR_SESSION_COOKIE/ s/localhost/$1/" './newsblur_web/docker_local_settings.py'
+     sed -i'.bak'  -e "/NEWSBLUR_URL/ s/localhost/$1/" -e "/SESSION_COOKIE_DOMAIN/ s/localhost/$1/" './newsblur_web/docker_local_settings.py'
      head -20 './newsblur_web/docker_local_settings.py'
      echo "$(tput setaf 1)Changinging config/fixtures/bootstrap.json:$(tput setaf 2) localhost to $1$(tput sgr 0)"
      sed -i'.bak' "/domain/ s/localhost/$1/" './config/fixtures/bootstrap.json'
@@ -15,7 +15,7 @@ if [ $# -eq 0 ]
       if [ $# -eq 2 ]
       then
        echo "$(tput setaf 1)Changinging newsblur_web/docker_local_settings.py: $(tput setaf 2)$1 to $2$(tput sgr 0)"
-       sed -i'bak' -e "/NEWSBLUR_URL/ s/$1/$2/" -e "/NEWSBLUR_SESSION_COOKIE/ s/$1/$2/" './newsblur_web/docker_local_settings.py' 
+       sed -i'bak' -e "/NEWSBLUR_URL/ s/$1/$2/" -e "/SESSION_COOKIE_DOMAIN/ s/$1/$2/" './newsblur_web/docker_local_settings.py' 
        head -20 './newsblur_web/docker_local_settings.py'
        echo "$(tput setaf 1)Changinging config/fixtures/bootstrap.json: $(tput setaf 2)$1 to $2$(tput sgr 0)"
        sed -i'bak' -e "/domain/ s/$1/$2/" './config/fixtures/bootstrap.json'
