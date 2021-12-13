be3237cdc3d9e10980cf90f3899357c31c840719
Samuel Clay <samuel@ofbrooklyn.com>
2021-12-13 12:45:47 -0500

    Updating to boto3 syntax.

--- utils/backups/backup_redis.py
+++ utils/backups/backup_redis.py
@@ -11,11 +11,10 @@ import time
 import boto3
 from django.conf import settings
 
-s3 = boto3.resource('s3') 
-bucket = s3.Bucket(settings.S3_BACKUP_BUCKET)
+s3 = boto3.client('s3') 
 
 hostname = socket.gethostname().replace('-','_')
 filename = f'backup_{hostname}/backup_{hostname}_{time.strftime("%Y-%m-%d-%H-%M")}.rdb.gz'
 path = '/var/lib/redis/dump.rdb'
 print('Uploading %s (from %s) to S3...' % (filename, path))
-bucket.upload_file(path, name=filename)
+s3.upload_file(path, settings.S3_BACKUP_BUCKET, filename)
