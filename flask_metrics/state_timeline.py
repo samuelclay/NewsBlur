99dd44a435a198f41209193f158afa6f17fc9fc8
Jonathan Math <jonathanamath@gmail.com>
2021-12-15 11:35:16 -0500

    move state timeline to one single flask that gets astate for all services

--- flask_metrics/state_timeline.py
+++ flask_metrics/state_timeline.py
@@ -1,7 +1,19 @@
-from flask import render_template
+from flask import Flask, render_template, Response
 import requests
 from requests.auth import HTTPBasicAuth
 
+from newsblur_web import settings
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+if settings.FLASK_SENTRY_DSN is not None:
+    sentry_sdk.init(
+        dsn=settings.FLASK_SENTRY_DSN,
+        integrations=[FlaskIntegration()],
+        traces_sample_rate=1.0,
+    )
+
+app = Flask(__name__)
 
 STATUS_MAPPING = {
     "UNK": 0,      # unknown
@@ -27,16 +39,32 @@ def format_state_data(label, data):
             formatted_data[k] = f'{label}{{servername="{k}"}} {STATUS_MAPPING[v]}'
     return formatted_data
 
-def get_state(backend_name):
+def get_state():
     res = requests.get('https://newsblur.com:1936/;csv', auth=HTTPBasicAuth('gimmiestats', 'StatsGiver'))
     lines = res.content.decode('utf-8').split('\n')
-    backends = [line.split(",") for line in lines if backend_name in line]
+    backends = [line.split(",") for line in lines][1:]
 
     check_status_index = lines[0].split(",").index('check_status')
     servername_index = lines[0].split(",").index('svname')
 
     data = {}
-
     for backend_data in backends:
-        data[backend_data[servername_index]] = backend_data[check_status_index].replace("*", "")
+        if backend_data != [''] and backend_data[servername_index] != 'FRONTEND':
+            data[backend_data[servername_index]] = backend_data[check_status_index].replace("*", "")
     return data
+
+@app.route("/state/")
+def state():
+    state_data = get_state()
+    formatted_data = format_state_data("state", state_data)
+    context = {
+        'chart_name': 'state',
+        'chart_type': 'gauge',
+        'data': formatted_data
+    }
+    html_body = render_template('prometheus_data.html', **context)
+    return Response(html_body, content_type="text/plain")
+
+if __name__ == "__main__":
+    print(" ---> Starting State Timeline Flask Metrics server...")
+    app.run(host="0.0.0.0", port=5569)
