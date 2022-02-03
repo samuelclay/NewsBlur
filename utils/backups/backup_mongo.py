d05c3b15995cafb3fb7d258adae5c7f9d49e578a
Samuel Clay <samuel@ofbrooklyn.com>
2022-02-03 14:24:12 -0500

    Removing unused backup code.

--- utils/backups/backup_mongo.py
+++ utils/backups/backup_mongo.py
@@ -115,16 +115,7 @@ def splitext( filename ):
 def upload(source_path, bucketname, keyname, acl='private', guess_mimetype=True):
 
     client = boto3.client('s3', aws_access_key_id=settings.S3_ACCESS_KEY, aws_secret_access_key=settings.S3_SECRET)
-    transfer = S3Transfer(client)
-    # Upload /tmp/myfile to s3://bucket/key
-    extra_args = {
-        'ACL': acl,
-    }
-    if guess_mimetype:
-        mtype = mimetypes.guess_type(keyname)[0] or 'application/octet-stream'
-        extra_args['ContentType'] = mtype
-
-    transfer.upload_file(source_path, bucketname, keyname, extra_args=extra_args, Callback=ProgressPercentage(source_path))
+    client.upload_file(source_path, bucketname, keyname, Callback=ProgressPercentage(source_path))
 
 
 class ProgressPercentage(object):
