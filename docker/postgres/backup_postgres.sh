386861a4795eb3c63224c3b1a1a41695113c1b66
Samuel Clay <samuel@ofbrooklyn.com>
2022-02-03 15:53:42 -0500

    Separating out restoring backups and storing backups.

--- docker/postgres/backup_postgres.sh
+++ docker/postgres/backup_postgres.sh
@@ -3,16 +3,13 @@
 now=$(date '+%Y-%m-%d-%H-%M')
 
 echo "---> PG dumping - ${now}"
-BACKUP_FILE="/srv/newsblur/backups/backup_postgresql_${now}.sql"
+BACKUP_FILE="/srv/newsblur/backup/backup_postgresql_${now}.sql"
 sudo docker exec -it postgres /usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc newsblur > $BACKUP_FILE
-
-echo " ---> Compressing $BACKUP_FILE"
-gzip $BACKUP_FILE
 
 echo " ---> Uploading postgres backup to S3"
 sudo docker run --user 1000:1001 --rm \
     -v /srv/newsblur:/srv/newsblur \
-    -v /srv/newsblur/backups/:/srv/newsblur/backups/ \
+    -v /srv/newsblur/backup/:/srv/newsblur/backup/ \
     --network=host \
     newsblur/newsblur_python3 \
     python /srv/newsblur/utils/backups/backup_psql.py
