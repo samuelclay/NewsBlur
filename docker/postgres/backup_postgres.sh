94fda89ee5b830e25ad237e1f18736b04621f96b
Samuel Clay <samuel@ofbrooklyn.com>
2022-04-29 17:13:17 -0400

    Paths are in different environments.

--- docker/postgres/backup_postgres.sh
+++ docker/postgres/backup_postgres.sh
@@ -2,7 +2,10 @@
 
 now=$(date '+%Y-%m-%d-%H-%M')
 BACKUP_PATH="/var/lib/postgresql/backup/"
-BACKUP_FILE="${BACKUP_PATH}backup_postgresql_${now}.sql"
+UPLOAD_PATH="/srv/newsblur/backups/"
+BACKUP_FILENAME="backup_postgresql_${now}.sql"
+BACKUP_FILE="${BACKUP_PATH}${BACKUP_FILENAME}"
+UPLOAD_FILE="${UPLOAD_PATH}${BACKUP_FILENAME}"
 echo "---> PG dumping - ${now}: ${BACKUP_FILE}"
 sudo docker exec -it postgres sh -c "mkdir -p $BACKUP_PATH"
 sudo docker exec -it postgres sh -c "/usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc newsblur > $BACKUP_FILE"
@@ -13,7 +16,7 @@ sudo docker run --user 1000:1001 --rm \
     -v /srv/newsblur:/srv/newsblur \
     --network=host \
     newsblur/newsblur_python3 \
-    python /srv/newsblur/utils/backups/backup_psql.py $BACKUP_FILE
+    python /srv/newsblur/utils/backups/backup_psql.py $UPLOAD_FILE
 
 # Don't delete backup since the backup_mongo.py script will rm them
 ## rm /opt/mongo/newsblur/backup/backup_mongo_${now}.tgz
