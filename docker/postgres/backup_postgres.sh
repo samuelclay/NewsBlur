464fecd18e8a412fe34615378c88324de301b151
Samuel Clay <samuel@ofbrooklyn.com>
2022-04-29 16:44:46 -0400

    Adding postgres restore command and updating backup script.

--- docker/postgres/backup_postgres.sh
+++ docker/postgres/backup_postgres.sh
@@ -2,18 +2,18 @@
 
 now=$(date '+%Y-%m-%d-%H-%M')
 
-echo "---> PG dumping - ${now}"
-BACKUP_FILE="/srv/newsblur/backup/backup_postgresql_${now}.sql"
-sudo docker exec -it postgres /usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc newsblur > $BACKUP_FILE
+BACKUP_FILE="/var/lib/postgresql/backup/backup_postgresql_${now}.sql"
+echo "---> PG dumping - ${now}: ${BACKUP_FILE}"
+sudo docker exec -it postgres sh -c '/usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc newsblur > ${BACKUP_FILE}'
 
 echo " ---> Uploading postgres backup to S3"
 sudo docker run --user 1000:1001 --rm \
     -v /srv/newsblur:/srv/newsblur \
     --network=host \
     newsblur/newsblur_python3 \
-    python /srv/newsblur/utils/backups/backup_psql.py
+    python /srv/newsblur/utils/backups/backup_psql.py $BACKUP_FILE
 
 # Don't delete backup since the backup_mongo.py script will rm them
 ## rm /opt/mongo/newsblur/backup/backup_mongo_${now}.tgz
 ## rm /opt/mongo/newsblur/backup/backup_mongo_${now}
-echo " ---> Finished uploading backups to S3: "
+echo " ---> Finished uploading backups to S3"
