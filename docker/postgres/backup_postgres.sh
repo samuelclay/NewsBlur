3670faf99d0b407f92ff600efda7e317de92e2da
Samuel Clay <samuel@ofbrooklyn.com>
2022-04-30 07:24:46 -0400

    Adding a secondary postgres that starts from a pg_basebackup. Still needs testing.

--- docker/postgres/backup_postgres.sh
+++ docker/postgres/backup_postgres.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 now=$(date '+%Y-%m-%d-%H-%M')
+BACKUP_FILENAME="backup_postgresql_${now}.sql"
 BACKUP_PATH="/var/lib/postgresql/backup/"
 UPLOAD_PATH="/srv/newsblur/backups/"
-BACKUP_FILENAME="backup_postgresql_${now}.sql"
 BACKUP_FILE="${BACKUP_PATH}${BACKUP_FILENAME}"
 UPLOAD_FILE="${UPLOAD_PATH}${BACKUP_FILENAME}"
+
 echo $(date -u) "---> PG dumping - ${now}: ${BACKUP_FILE}"
 sudo docker exec postgres sh -c "mkdir -p $BACKUP_PATH"
 sudo docker exec postgres sh -c "/usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc newsblur > $BACKUP_FILE"
