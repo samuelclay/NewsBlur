de309183c5a2f8df3d977de49df2b5b670a74d21
Samuel Clay <samuel@ofbrooklyn.com>
2022-04-30 06:51:11 -0400

    Adding timestamps to backup logs

--- docker/postgres/backup_postgres.sh
+++ docker/postgres/backup_postgres.sh
@@ -6,12 +6,12 @@ UPLOAD_PATH="/srv/newsblur/backups/"
 BACKUP_FILENAME="backup_postgresql_${now}.sql"
 BACKUP_FILE="${BACKUP_PATH}${BACKUP_FILENAME}"
 UPLOAD_FILE="${UPLOAD_PATH}${BACKUP_FILENAME}"
-echo "---> PG dumping - ${now}: ${BACKUP_FILE}"
+echo $(date -u) "---> PG dumping - ${now}: ${BACKUP_FILE}"
 sudo docker exec postgres sh -c "mkdir -p $BACKUP_PATH"
 sudo docker exec postgres sh -c "/usr/lib/postgresql/13/bin/pg_dump -U newsblur -h 127.0.0.1 -Fc newsblur > $BACKUP_FILE"
 
 
-echo " ---> Uploading postgres backup to S3"
+echo $(date -u) " ---> Uploading postgres backup to S3"
 sudo docker run --user 1000:1001 --rm \
     -v /srv/newsblur:/srv/newsblur \
     --network=host \
@@ -21,4 +21,4 @@ sudo docker run --user 1000:1001 --rm \
 # Don't delete backup since the backup_mongo.py script will rm them
 ## rm /opt/mongo/newsblur/backup/backup_mongo_${now}.tgz
 ## rm /opt/mongo/newsblur/backup/backup_mongo_${now}
-echo " ---> Finished uploading backups to S3"
+echo $(date -u) " ---> Finished uploading backups to S3"
