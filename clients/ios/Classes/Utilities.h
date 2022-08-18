bc35c84bf7ede19ef88d84c9535ff94e93c12881
David Sinclair <dev@dejal.com>
2022-08-17 21:48:20 -0700

    #1693 (2022 Redesign)
    - Replaced the feeds list notificaitons icon.
    - The feed detail folder title icon now matches the color.
    - Changed all of the settings icons to the new style.
    - Changed the feed detail mark read icon.
    - Changed the focus and hidden unread indicators.
    - Unread indicators sized to match the web.
    - Tweaked feed detail spacing.
    - Increased feed detail titles to up to three lines.
    - Titlecased Interactions / Your Activities.
    - Removed borders and tweaked section titles in trainer.

--- clients/ios/Classes/Utilities.h
+++ clients/ios/Classes/Utilities.h
@@ -19,7 +19,7 @@ void drawLinearGradient(CGContextRef context, CGRect rect, CGColorRef startColor
 + (UIImage *)roundCorneredImage:(UIImage *)orig radius:(CGFloat)r;
 + (UIImage *)roundCorneredImage: (UIImage*)orig radius:(CGFloat)r convertToSize:(CGSize)size;
 + (UIImage *)templateImageNamed:(NSString *)imageName sized:(CGFloat)size;
-+ (UIImage *)imageNamed:(NSString *)imageName withSize:(CGSize)size;
++ (UIImage *)imageNamed:(NSString *)imageName sized:(CGFloat)size;
 + (UIImage *)imageWithImage:(UIImage *)image convertToSize:(CGSize)size;
 + (NSString *)md5:(NSString *)string;
 + (NSString *)formatLongDateFromTimestamp:(NSInteger)timestamp;
