246d3f47c5aa56218ad552a242e804ed5771cb10
Samuel Clay <samuel@ofbrooklyn.com>
2022-11-26 12:01:53 -0500

    Show redis db name in log.

--- utils/redis_raw_log_middleware.py
+++ utils/redis_raw_log_middleware.py
@@ -88,16 +88,16 @@ class RedisDumpMiddleware(object):
             if isinstance(arg, Redis):
                 redis_connection = arg
                 redis_server_name = redis_connection.connection_pool.connection_kwargs['host']
-                if 'db-redis-user' in redis_server_name:
-                    redis_server_name = 'redis_user'
-                elif 'db-redis-session' in redis_server_name:
-                    redis_server_name = 'redis_session'
-                elif 'db-redis-story' in redis_server_name:
-                    redis_server_name = 'redis_story'
-                elif 'db-redis-pubsub' in redis_server_name:
-                    redis_server_name = 'redis_pubsub'
-                elif 'db_redis' in redis_server_name:
-                    redis_server_name = 'redis_user'
+                # if 'db-redis-user' in redis_server_name:
+                #     redis_server_name = 'redis_user'
+                # elif 'db-redis-session' in redis_server_name:
+                #     redis_server_name = 'redis_session'
+                # elif 'db-redis-story' in redis_server_name:
+                #     redis_server_name = 'redis_story'
+                # elif 'db-redis-pubsub' in redis_server_name:
+                #     redis_server_name = 'redis_pubsub'
+                # elif 'db_redis' in redis_server_name:
+                #     redis_server_name = 'redis_user'
                 continue
             if len(str(arg)) > 100:
                 arg = "[%s bytes]" % len(str(arg))
