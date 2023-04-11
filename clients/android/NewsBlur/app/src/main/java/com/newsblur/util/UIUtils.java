349d52e61916fdbaa64a1b92b20551bc80296c86
sictiru <sictiru@users.noreply.github.com>
2023-04-10 19:34:11 -0700

    Import

--- clients/android/NewsBlur/app/src/main/java/com/newsblur/util/UIUtils.java
+++ clients/android/NewsBlur/app/src/main/java/com/newsblur/util/UIUtils.java
@@ -36,6 +36,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabColorSchemeParams;
