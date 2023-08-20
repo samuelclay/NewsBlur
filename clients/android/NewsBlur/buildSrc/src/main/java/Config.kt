cd36bd398ad752e6cf61d58e88221d4af38d2136
Samuel Clay <samuel@ofbrooklyn.com>
2023-08-20 16:03:10 -0400

    Android v13.1.0

--- clients/android/NewsBlur/buildSrc/src/main/java/Config.kt
+++ clients/android/NewsBlur/buildSrc/src/main/java/Config.kt
@@ -5,8 +5,8 @@ object Config {
     const val compileSdk = 33
     const val minSdk = 23
     const val targetSdk = 33
-    const val versionCode = 212
-    const val versionName = "13.0.0"
+    const val versionCode = 213
+    const val versionName = "13.1.0"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 
