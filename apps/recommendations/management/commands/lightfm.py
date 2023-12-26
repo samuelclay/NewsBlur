02494c15279e731d7b5c9ce60dcfc55270168bc8
Samuel Clay <samuel@ofbrooklyn.com>
2023-12-26 11:07:37 -0500

    Using KNN for recs. I think this might have actually worked.

--- apps/recommendations/management/commands/lightfm.py
+++ apps/recommendations/management/commands/lightfm.py
@@ -3,8 +3,10 @@ import pandas as pd
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from lightfm import LightFM
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, csr_matrix
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
 
 from apps.rss_feeds.models import Feed
 
@@ -50,35 +52,32 @@ class Command(BaseCommand):
             self.stdout.write("Training data...")
             model.fit(user_feed_matrix, epochs=30, num_threads=2)
 
-            def recommend_for_all_users_for_feed(feed_id, user_feed_matrix, feed_subscribers, n_items=10):
-                # Apply TF-IDF transformation
-                transformer = TfidfTransformer()
-                tfidf_matrix = transformer.fit_transform(user_feed_matrix)
+            def calculate_similarity(user_feed_matrix):
+                # Ensure the matrix is in CSR format for efficient row-wise operations
+                if not isinstance(user_feed_matrix, csr_matrix):
+                    user_feed_matrix = csr_matrix(user_feed_matrix)
 
-                # Calculate the sum of tfidf scores for each feed across all users
-                feed_scores = tfidf_matrix.sum(axis=0)
+                # Compute the cosine similarity matrix (result is dense, handle with care)
+                similarity_matrix = cosine_similarity(user_feed_matrix.T, dense_output=False)
+                return similarity_matrix
 
-                # Convert to a 1D numpy array
-                feed_scores = np.array(feed_scores).squeeze()
+            def recommend_feeds_knn(feed_id, user_feed_matrix, n_items=10):
+                model_knn = NearestNeighbors(
+                    metric="cosine", algorithm="brute", n_neighbors=n_items, n_jobs=-1
+                )
 
-                # Adjust the scores based on the number of subscribers
-                for idx, score in enumerate(feed_scores):
-                    # Apply some adjustment based on feed_subscribers
-                    # For example, you could decrease the score for feeds with many subscribers
-                    feed_scores[idx] = score / (1 + np.log1p(feed_subscribers[idx]))
+                # Fit the model
+                model_knn.fit(user_feed_matrix.T)  # Transpose to get feed-wise neighbors
 
-                # Sort feeds based on adjusted scores
-                top_feeds_indices = np.argsort(-feed_scores)
+                # Find neighbors for the specified feed
+                distances, indices = model_knn.kneighbors(
+                    user_feed_matrix.T[feed_id], n_neighbors=n_items + 1
+                )
 
-                # Build the list of recommended feeds
-                top_feeds = []
-                for idx in top_feeds_indices:
-                    if len(top_feeds) >= n_items:
-                        break
-                    if idx != feed_id:
-                        top_feeds.append(idx)
+                # Exclude the feed itself and return the indices of recommended feeds
+                recommended_feeds = [idx for idx in indices[0] if idx != feed_id]
 
-                return top_feeds
+                return recommended_feeds[:n_items]
 
             # Recommend for a specific feed ID
             feed_id = options["feed"]
@@ -86,16 +85,11 @@ class Command(BaseCommand):
                 self.style.SUCCESS(f"Generating recommendations for feed: {Feed.get_by_id(feed_id)}")
             )
             # Create a NumPy array for the number of subscribers per feed
-            max_feed_id = data["feed_id"].max()
-            feed_subscribers = np.zeros(max_feed_id + 1)
-            for _, row in data.iterrows():
-                feed_subscribers[int(row["feed_id"])] = row["num_subscribers"]
-
-            # Call the recommendation function with this data
-            top_recommended_feeds = recommend_for_all_users_for_feed(
-                feed_id, user_feed_matrix, feed_subscribers
+            similarity_matrix = calculate_similarity(user_feed_matrix)
+            top_recommended_feeds = recommend_feeds_knn(feed_id, similarity_matrix)
+            self.stdout.write(
+                f"Found {len(top_recommended_feeds)} recommendations for feed ID {feed_id}: {top_recommended_feeds}"
             )
-            self.stdout.write(f"Found {len(top_recommended_feeds)} recommendations for feed ID {feed_id}")
 
             for feed_id in top_recommended_feeds:
                 feed = Feed.get_by_id(feed_id)
