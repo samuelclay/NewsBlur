43842c7120127fc9309edcac8796445790b4e6c6
sictiru <sictiru@users.noreply.github.com>
2024-03-10 20:28:39 -0700

    Update dependencies

--- clients/android/NewsBlur/build.gradle.kts
+++ clients/android/NewsBlur/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id(Plugins.androidApplication) version Version.android apply false
     id(Plugins.androidLibrary) version Version.android apply false
@@ -17,4 +19,17 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
