192e94df529e538ac5d3a983d8cfd5e3006cae1d
sictiru <sictiru@users.noreply.github.com>
2024-06-16 18:31:21 -0700

    Android v13.2.6

--- clients/android/NewsBlur/buildSrc/src/main/java/Config.kt
+++ clients/android/NewsBlur/buildSrc/src/main/java/Config.kt
@@ -5,8 +5,8 @@ object Config {
     const val compileSdk = 34
     const val minSdk = 24
     const val targetSdk = 34
-    const val versionCode = 221
-    const val versionName = "13.2.5"
+    const val versionCode = 222
+    const val versionName = "13.2.6"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 
