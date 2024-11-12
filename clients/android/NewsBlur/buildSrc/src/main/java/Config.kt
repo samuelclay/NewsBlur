80fb506e6ad1d691fea8072ac3308bcaf569fcf2
sictiru <sictiru@users.noreply.github.com>
2024-11-11 20:56:47 -0800

    #1886 Support for themed icons

--- clients/android/NewsBlur/buildSrc/src/main/java/Config.kt
+++ clients/android/NewsBlur/buildSrc/src/main/java/Config.kt
@@ -3,12 +3,12 @@ import org.gradle.api.JavaVersion
 object Config {
 
     const val compileSdk = 34
-    const val minSdk = 24
+    const val minSdk = 26
     const val targetSdk = 34
     const val versionCode = 229
     const val versionName = "13.3.1"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
 
-    val javaVersion = JavaVersion.VERSION_17
+    val javaVersion = JavaVersion.VERSION_21
 }
