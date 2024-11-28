beb33f2577567595650f66f04200961a8a05247c
Samuel Clay <samuel@ofbrooklyn.com>
2024-11-28 14:45:41 -0500

    Moving NEWSBLUR.Collections.DiscoverStories to its proper place.

--- media/js/newsblur/models/stories.js
+++ media/js/newsblur/models/stories.js
@@ -719,20 +719,3 @@ NEWSBLUR.Collections.Stories = Backbone.Collection.extend({
     }
 
 });
-
-NEWSBLUR.Collections.DiscoverStories = NEWSBLUR.Collections.Stories.extend({
-
-    url: function () {
-        var url = '/rss_feeds/discover/stories/' + this.similar_to_story_hash + '/';
-        if (this.feed_ids && this.feed_ids.length > 0) {
-            url += '?feed_id=' + this.feed_ids.join("&feed_id=");
-        }
-
-        return url;
-    },
-
-    parse: function (response) {
-        return response.discover_stories;
-    }
-
-});
