bd615cc84e2ec1d701abfee83dbfaa2af595f3e4
sictiru <sictiru@users.noreply.github.com>
2025-07-11 09:08:53 -0700

    #1958 Dependencies warmup

--- clients/android/NewsBlur/app/src/main/java/com/newsblur/NbApplication.kt
+++ clients/android/NewsBlur/app/src/main/java/com/newsblur/NbApplication.kt
@@ -6,32 +6,61 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.preference.PreferenceManager
+import com.newsblur.database.BlurDatabaseHelper
+import com.newsblur.di.IconFileCache
+import com.newsblur.di.StoryImageCache
+import com.newsblur.di.ThumbnailCache
 import com.newsblur.preference.PrefsRepo
+import com.newsblur.util.FileCache
 import com.newsblur.util.Log
 import com.newsblur.util.PrefConstants
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import javax.inject.Provider
 
 @HiltAndroidApp
 class NbApplication : Application(), DefaultLifecycleObserver {
 
     @Inject
-    lateinit var prefsRepo: PrefsRepo
+    @IconFileCache
+    lateinit var iconCacheProvider: Provider<FileCache>
+
+    @Inject
+    @ThumbnailCache
+    lateinit var thumbnailCacheProvider: Provider<FileCache>
+
+    @Inject
+    @StoryImageCache
+    lateinit var storyImageCacheProvider: Provider<FileCache>
+
+    @Inject
+    lateinit var prefsRepo: Provider<PrefsRepo>
+
+    @Inject
+    lateinit var dbHelper: Provider<BlurDatabaseHelper>
 
     override fun onCreate() {
         super<Application>.onCreate()
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
         Log.offerContext(this)
 
-        // load SharedPreferences from disk
-        getSharedPreferences(PrefConstants.PREFERENCES, 0)
+        // warm up most of the dependencies that would block the UI thread
+        CoroutineScope(Dispatchers.IO).launch {
+            dbHelper.get()
+            prefsRepo.get()
+            iconCacheProvider.get()
+            thumbnailCacheProvider.get()
+            storyImageCacheProvider.get()
 
-        // load XML default and write any missing keys
-        PreferenceManager.setDefaultValues(
-                this@NbApplication,
-                R.xml.activity_settings,
-                false,
-        )
+            PreferenceManager.setDefaultValues(
+                    this@NbApplication,
+                    R.xml.activity_settings,
+                    false,
+            )
+        }
 
         if (BuildConfig.DEBUG) {
 //            StrictMode.setVmPolicy(
