fbc1d0d801768af5bdfe02cf67b1eb5dc9d8e00e
Samuel Clay <samuel@ofbrooklyn.com>
2025-12-25 20:45:41 -0500

    (Quiet) launching Ask AI to free and premium users, who have weekly limits.

--- media/js/newsblur/views/story_detail_view.js
+++ media/js/newsblur/views/story_detail_view.js
@@ -243,11 +243,7 @@ NEWSBLUR.Views.StoryDetailView = Backbone.View.extend({
             show_sideoption_save: NEWSBLUR.assets.preference("show_sideoption_save"),
             show_sideoption_share: NEWSBLUR.assets.preference("show_sideoption_share"),
             show_sideoption_related: NEWSBLUR.assets.preference("show_sideoption_related"),
-            show_sideoption_ask_ai: NEWSBLUR.assets.preference("show_sideoption_ask_ai") && (
-                NEWSBLUR.Globals.is_staff ||
-                NEWSBLUR.Globals.is_archive ||
-                (NEWSBLUR.Globals.debug && NEWSBLUR.Globals.is_premium)
-            ),
+            show_sideoption_ask_ai: NEWSBLUR.assets.preference("show_sideoption_ask_ai"),
             sharing_services: NEWSBLUR.assets.third_party_sharing_services,
         };
     },
