3840275b699749a2586ac183564981e822c743e4
Samuel Clay <samuel@ofbrooklyn.com>
2026-09-17 08:29:14 -0700

    Prepare Android 15.0.2 with restored story long-press menu

--- clients/android/NewsBlur/app/build.gradle.kts
+++ clients/android/NewsBlur/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
             libs.versions.targetSdk
                 .get()
                 .toInt()
-        versionCode = 286
-        versionName = "15.0.1"
+        versionCode = 287
+        versionName = "15.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["defaultLauncherEnabled"] = "true"
